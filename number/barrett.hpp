bbb174cd5091b4af653b8b952504b1e2525caf53
prime3372 <syunrurusera@yahoo.ne.jp>
2026-06-19 10:52:23 +0900

    make barrett constexpr

--- number/barrett.hpp
+++ number/barrett.hpp
@@ -3,11 +3,11 @@
 // Barrett reduction
 struct barrett {
 public:
-  explicit barrett(unsigned int _m) : m(_m), im((unsigned long long)(-1) / _m + 1) {}
+  explicit constexpr barrett(unsigned int _m) : m(_m), im((unsigned long long)(-1) / _m + 1) {}
 
-  unsigned int umod() const { return m; }
+  constexpr unsigned int umod() const { return m; }
 
-  unsigned int mul(unsigned int a, unsigned int b) const {
+  constexpr unsigned int mul(unsigned int a, unsigned int b) const {
     unsigned long long z = a;
     z *= b;
     unsigned long long x = (unsigned long long)(((unsigned __int128)(z) * im) >> 64);
--- number/pow_mod.hpp
+++ number/pow_mod.hpp
@@ -9,27 +9,16 @@
 template <std::integral Mod> requires (sizeof(Mod) <= 4)
 constexpr long long pow_mod(long long x, long long n, Mod m) {
   assert(0 <= n && 1 <= m);
-  if consteval {
-    long long r = 1;
-    long long y = x < 0 ? x % m + m : x % m;
-    while (n) {
-      if (n & 1) r = r * y % m;
-      y = y * y % m;
-      n >>= 1;
-    }
-    return r;
-  } else {
-    if (m == 1) return 0;
-    barrett bt((unsigned int)(m));
-    x = x < 0 ? x % m + m : x % m;
-    unsigned int r = 1, y = (unsigned int)(x);
-    while (n) {
-      if (n & 1) r = bt.mul(r, y);
-      y = bt.mul(y, y);
-      n >>= 1;
-    }
-    return r;
+  if (m == 1) return 0;
+  barrett bt((unsigned int)(m));
+  x = x < 0 ? x % m + m : x % m;
+  unsigned int r = 1, y = (unsigned int)(x);
+  while (n) {
+    if (n & 1) r = bt.mul(r, y);
+    y = bt.mul(y, y);
+    n >>= 1;
   }
+  return r;
 }
 
 constexpr long long pow_mod(long long x, long long n, long long m) {
