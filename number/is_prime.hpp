3df7619d8bddc5fbb1c8483fa361546c52d3794f
prime3372 <syunrurusera@yahoo.ne.jp>
2026-06-21 03:04:15 +0900

    modify is_prime.hpp

--- number/is_prime.hpp
+++ number/is_prime.hpp
@@ -1,9 +1,35 @@
 #pragma once
 
+#include <concepts>
+#include "barrett.hpp"
 #include "pow_mod.hpp"
 
 // M. Forisek and J. Jancina,
 // Fast Primality Testing for Integers That Fit into a Machine Word
+template <std::integral T> requires(sizeof(T) <= 4)
+constexpr bool is_prime(T n) {
+  if (n <= 2) return n == 2;
+  if (n % 2 == 0) return false; 
+  long long d = (long long)(n) - 1;
+  while (d % 2 == 0) d /= 2;
+  barrett bt((unsigned int)(n));
+  for (long long a : {2, 7, 61}) {
+    if (a % n == 0) continue;
+    long long y = pow_mod(a, d, n);
+    if (y == 1) continue;
+    bool ok = false;
+    for (long long t = d; t != (long long)(n) - 1; t *= 2) {
+      if (y == (long long)(n) - 1) {
+        ok = true;
+        break;
+      }
+      y = bt.mul(y, y);
+    }
+    if (!ok) return false;
+  }
+  return true;
+}
+
 constexpr bool is_prime(long long n) {
   if (n <= 2) return n == 2;
   if (n % 2 == 0) return false; 
