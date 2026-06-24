057503d2164ff2d2bdd7669b49a7ad15dab0de19
prime3372 <syunrurusera@yahoo.ne.jp>
2026-06-25 04:41:33 +0900

    add double_ended_priority_queue

---
+++ ds/double_ended_priority_queue.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+// even index -> min heap, odd index -> max heap
+template <class T> struct double_ended_priority_queue {
+
+  double_ended_priority_queue() : d(2) {}
+
+  T min() const {
+    assert(!empty());
+	  return d[2];
+  }
+  T max() const {
+    assert(!empty());
+	  return int(d.size()) == 3 ? d[2] : d[3];
+  }
+
+  void push(T x) {
+	  d.push_back(x);
+	  up(int(d.size()) - 1);
+  }
+
+  void pop_min() {
+    assert(!empty());
+	  if (size() == 1) {
+	    d.pop_back();
+      return;
+	  }
+    std::swap(d[2], d.back());
+    d.pop_back();
+    up(down(2));
+  }
+  void pop_max() {
+    assert(!empty());
+	  if (size() <= 2) {
+	    d.pop_back(); 
+      return;
+	  }
+    std::swap(d[3], d.back());
+    d.pop_back();
+    up(down(3));
+  }
+
+  int size() const { return int(d.size()) - 2; }
+  bool empty() const { return size() == 0; }
+
+private:
+  std::vector<T> d;
+
+  int down(int cur) {
+	  int n = int(d.size());
+    if (cur == 2) {
+      while (true) {
+        int nxt = 2 * cur;
+        if (nxt + 2 < n && d[nxt] > d[nxt + 2]) nxt += 2;
+        if (nxt < n && d[cur] > d[nxt]) {
+          std::swap(d[cur], d[nxt]);
+          cur = nxt;
+        } else break;
+      }
+    } else {
+      while (true) {
+        int nxt = 2 * (cur - 1) + 1;
+        if (nxt + 2 < n && d[nxt] < d[nxt + 2]) nxt += 2;
+        if (nxt < n && d[cur] < d[nxt]) {
+          std::swap(d[cur], d[nxt]);
+          cur = nxt;
+        } else break;
+      }
+    }
+	  return cur;
+  }
+
+  int up(int cur) {
+    int n = int(d.size());
+	  if ((cur | 1) < n && d[cur & ~1] > d[cur | 1]) {
+	    std::swap(d[cur & ~1], d[cur | 1]);
+	    cur ^= 1;
+	  }
+    if (d[cur] < d[cur / 4 * 2]) {
+      while (true) {
+        int nxt = cur / 4 * 2;
+        if (cur >= 4 && d[cur] < d[nxt]) {
+        std::swap(d[cur], d[nxt]);
+        cur = nxt;
+        } else break;
+      }
+    } else {
+      while (true) {
+        int nxt = cur / 4 * 2 + 1;
+        if (cur >= 4 && d[cur] > d[nxt]) {
+          std::swap(d[cur], d[nxt]);
+          cur = nxt;
+        } else break;
+      }
+
+    }
+    return cur;
+  }
+};
---
+++ test/yosupo/double_ended_priority_queue.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/double_ended_priority_queue"
+
+#include "../../ds/double_ended_priority_queue.hpp"
+#include <bits/stdc++.h>
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  double_ended_priority_queue<int> pq;
+  rep(i, 0, n) {
+    int si;
+    cin >> si;
+    pq.push(si);
+  }
+  while (q--) {
+    int t;
+    cin >> t;
+    if (t == 0) {
+      int x;
+      cin >> x;
+      pq.push(x);
+    } else if (t == 1) {
+      cout << pq.min() << "\n";
+      pq.pop_min();
+    } else {
+      cout << pq.max() << "\n";
+      pq.pop_max();
+    }
+  }
+}
