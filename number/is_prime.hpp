e256a7544e1518fd2510ce62a155d7e339d28677
prime3372 <syunrurusera@yahoo.ne.jp>
2026-06-25 20:06:51 +0900

    add blank lines

--- number/is_prime.hpp
+++ number/is_prime.hpp
@@ -10,13 +10,15 @@ template <std::integral T> requires(sizeof(T) <= 4)
 constexpr bool is_prime(T n) {
   if (n <= 2) return n == 2;
   if (n % 2 == 0) return false; 
+  barrett bt((unsigned int)(n));
   long long d = (long long)(n) - 1;
   while (d % 2 == 0) d /= 2;
-  barrett bt((unsigned int)(n));
+
   for (long long a : {2, 7, 61}) {
     if (a % n == 0) continue;
     long long y = pow_mod(a, d, n);
     if (y == 1) continue;
+
     bool ok = false;
     for (long long t = d; t != (long long)(n) - 1; t *= 2) {
       if (y == (long long)(n) - 1) {
@@ -35,10 +37,12 @@ constexpr bool is_prime(long long n) {
   if (n % 2 == 0) return false; 
   long long d = n - 1;
   while (d % 2 == 0) d /= 2;
+
   for (long long a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
     if (a % n == 0) continue;
     __int128 y = pow_mod(a, d, n);
     if (y == 1) continue;
+
     bool ok = false;
     for (long long t = d; t != n - 1; t *= 2) {
       if (y == n - 1) {
