e6272a414f5cf65f0f4afd29ab62ddf4c997bffe
prime3372 <syunrurusera@yahoo.ne.jp>
2026-06-27 03:21:56 +0900

    add comment

--- number/sqrt_mod.hpp
+++ number/sqrt_mod.hpp
@@ -4,29 +4,43 @@
 
 // Cipolla's Algorithm
 // https://en.wikipedia.org/wiki/Cipolla%27s_algorithm
-constexpr long long sqrt_mod(long long a, long long p) {
-  a = a < 0 ? a % p + p : a % p;
-  if (a <= 1) return a;
-  if (pow_mod(a, (p - 1) / 2, p) != 1) return -1;
+constexpr long long sqrt_mod(long long n, long long p) {
+  n = n < 0 ? n % p + p : n % p;
+  if (n <= 1) return n;
+  if (pow_mod(n, (p - 1) / 2, p) != 1) return -1;
+  // if n is not a square in F_p, return -1
 
-  long long b = 0;
-  while (pow_mod(b * b - a, (p - 1) / 2, p) == 1) b++;
-  long long c = (b * b - a) % p;
-  if (c < 0) c += p;
+  long long a = 0;
+  while (pow_mod(a * a - n, (p - 1) / 2, p) == 1) a++;
+  // a*a - n is not a square in F_p
+  // so let w = sqrt(a*a - n) and F_p^2 = F_p(w)
+  long long w2 = (a * a - n) % p; 
+  if (w2 < 0) w2 += p;
 
-  struct K {
+  struct Fp2 {
     __int128 re, im;
   };
-  auto mul = [&](K x, K y) -> K {
-    __int128 re = (x.re * y.re + c * x.im % p * y.im) % p;
+  auto mul = [&](Fp2 x, Fp2 y) -> Fp2 {
+    __int128 re = (x.re * y.re + w2 * x.im % p * y.im) % p;
     __int128 im = (x.re * y.im + y.re * x.im) % p;
-    return K{re, im};
+    return Fp2{re, im};
   };
 
-  K x = {1, 0}, t = {b, 1};
+  Fp2 z = {1, 0}, t = {a, 1};
   for (int e = (p + 1) / 2; e; e >>= 1) {
-    if (e & 1) x = mul(x, t);
+    if (e & 1) z = mul(z, t);
     t = mul(t, t);
   }
-  return (long long)(x.re);
+  // [1] a^p = a (mod p) ...Fermat's little theorem
+  // [2] w^(p - 1) = (w^2)^{(p - 1)/2} = -1 (mod p) ...Euler's criterion
+  // [3] (x + yw)^p = x^p + y^pw^p (mod p) ...Freshman's dream
+  // -> (a + w)^p = a^p - w^p = a - w (mod p)
+
+  // z = (a + w)^{(p + 1)/2} (in F_p^2)
+  // z^2 = (a + w)^(p + 1) = (a + w)(a + w)^p = (a + w)(a - w) = a^2 - w^2 = n (in F_p^2)
+  // therefore z, -z are sqrt(n) in F_p^2
+  // polynomial of degree 2 has at most 2 roots in any field K, so z, -z are also sqrt(n) in F_p
+  // (note that we have already checked that n is a square in F_p)
+
+  return (long long)(z.re);
 }
