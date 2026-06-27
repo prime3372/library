5db86dbfddece4684fa6ae0f2730816d42eb0929
prime3372 <syunrurusera@yahoo.ne.jp>
2026-06-27 19:18:19 +0900

    fix

--- test/yosupo/static_range_sum.test.cpp
+++ test/yosupo/static_range_sum.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
 
-#include "../archive/cumsum.hpp"
+#include "../../archive/cumsum.hpp"
 #include <bits/stdc++.h>
 
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
