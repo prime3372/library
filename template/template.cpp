e1dbac38deea02dfad846dd600d6a5047bc92153
prime3372 <syunrurusera@yahoo.ne.jp>
2026-06-27 18:54:03 +0900

    create template/template.cpp

---
+++ template/template.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = int(a); i < int(b); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
