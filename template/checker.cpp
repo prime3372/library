1343b5509642ccf6ca890f82416a8943ebbe6083
prime3372 <syunrurusera@yahoo.ne.jp>
2026-06-30 12:38:15 +0900

    create checker template

---
+++ template/checker.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = int(a); i < int(b); i++)
+using namespace std;
+using ll = long long;
+#define check(expr) do { \ 
+                      if (!(expr)) { \
+                        std::cout << "wrong answer " << #expr << " is not satisfied" << std::endl; \
+                        std::exit(1); \
+                      } \
+                    } while (false)
+
+int main(int argc, char* argv[]) {
+  std::ifstream f_in(argv[1]);
+  std::ifstream f_out(argv[2]);
+  std::ifstream f_ans(argv[3]);
+}
