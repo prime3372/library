cd90b4acbd9cae1c23b5f85f2f37afce27b24a18
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-02 14:31:20 +0900

    create random_forest.hpp

--- random/random_forest.hpp
+++ random/random_forest.hpp
@@ -6,16 +6,16 @@
 #include <vector>
 #include "rng.hpp"
 
-std::vector<std::pair<int, int>> random_tree(int n) {
+std::vector<std::pair<int, int>> random_forest(int n) {
   assert(1 <= n);
   if (n == 1) return {};
 
   std::vector<int> par(n);
-  for (int i = 1; i < n; i++) par[i] = mt32() % i;
+  for (int i = 0; i < n; i++) par[i] = mt32() % (i + 1);
   
   std::vector<std::pair<int, int>> edges(n - 1);
-  for (int i = 1; i < n; i++) {
-    edges[i - 1] = {i, par[i]};
+  for (int i = 0; i < n; i++) {
+    if (par[i] != i) edges[i] = {i, par[i]};
   }
 
   std::shuffle(edges.begin(), edges.end(), mt32);
