21b1d628c43c4ba207392bbda19258ce4dbcab41
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-02 13:50:10 +0900

    fix

--- template/checker.cpp
+++ template/checker.cpp
@@ -2,7 +2,7 @@
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
 using namespace std;
 using ll = long long;
-#define check(expr) do { \ 
+#define check(expr) do { \
                       if (!(expr)) { \
                         std::cout << "wrong answer " << #expr << " is not satisfied" << std::endl; \
                         std::exit(1); \
@@ -10,6 +10,7 @@ using ll = long long;
                     } while (false)
 
 int main(int argc, char* argv[]) {
+  assert(argc >= 4);
   std::ifstream f_in(argv[1]);
   std::ifstream f_out(argv[2]);
   std::ifstream f_ans(argv[3]);
