75638c7eace603b94956bb3c657b259d9619ba78
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-04 23:59:52 +0900

    remove std::

--- template/checker.cpp
+++ template/checker.cpp
@@ -8,16 +8,16 @@ using mint = modint998244353;
 
 #define check(expr) do { \
                       if (!(expr)) { \
-                        std::cout << "wrong answer " << #expr << " is not satisfied" << std::endl; \
-                        std::exit(1); \
+                        cout << "wrong answer " << #expr << " is not satisfied" << endl; \
+                        exit(1); \
                       } \
                     } while (false)
 
 int main(int argc, char* argv[]) {
   assert(argc >= 4);
-  std::ifstream f_in(argv[1]);
-  std::ifstream f_out(argv[2]);
-  std::ifstream f_ans(argv[3]);
+  ifstream f_in(argv[1]);
+  ifstream f_out(argv[2]);
+  ifstream f_ans(argv[3]);
 
   {
     string dummy;
