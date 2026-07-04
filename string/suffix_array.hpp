9f78f01fb9d8ace8059cda3d5d73838bdea13662
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-05 03:10:18 +0900

    remove unnecessary processes

--- string/suffix_array.hpp
+++ string/suffix_array.hpp
@@ -7,51 +7,52 @@
 namespace cp {
 
 // https://cp-algorithms.com/string/suffix-array.html O(NlogN) approach
+// note: index[] and rank[] are used instead of p[] and c[]
 template <class Str> std::vector<int> suffix_array(const Str& s) {
   int n = int(s.size()) + 1;
-  std::vector<int> a(n);
-  Str t = s;
-  std::sort(t.begin(), t.end());
-  auto last = std::unique(t.begin(), t.end());
-  for (int i = 0; i < n - 1; i++) {
-    a[i] = int(std::lower_bound(t.begin(), last, s[i]) - t.begin()) + 1;
-  }
-  a[n - 1] = 0;
 
-  std::vector<int> index(n), rank(n), cnt(n, 0);
-  for (int i = 0; i < n; i++) cnt[a[i]]++;  
+  std::vector<int> rank(n);
+  int num_of_ranks;
+  {
+    Str t = s;
+    std::sort(t.begin(), t.end());
+    auto last = std::unique(t.begin(), t.end());
+    for (int i = 0; i < n - 1; i++) {
+      rank[i] = int(std::lower_bound(t.begin(), last, s[i]) - t.begin()) + 1;
+    }
+    rank[n - 1] = 0;
+    num_of_ranks = int(t.size());
+  }
+
+  std::vector<int> index(n), cnt(n, 0);
+  for (int i = 0; i < n; i++) cnt[rank[i]]++;
   for (int i = 1; i < n; i++) cnt[i] += cnt[i - 1];
-  for (int i = 0; i < n; i++) index[--cnt[a[i]]] = i;  
-
-  int num_of_ranks = 1;
-  rank[index[0]] = 0;
-  for (int i = 1; i < n; i++) {
-    if (a[index[i]] != a[index[i - 1]]) num_of_ranks++;
-    rank[index[i]] = num_of_ranks - 1;
-  }
+  for (int i = 0; i < n; i++) index[--cnt[rank[i]]] = i; // order[i] = --cnt[rank[i]]
 
   std::vector<int> nindex(n), nrank(n);
   for (int step = 0; (1 << step) < n; step++) {
-    for (int i = 0; i < n; i++) {
-      index[i] -= 1 << step;
-      if (index[i] < 0) index[i] += n;
-    }
+    int w = 1 << step;
+
     std::fill(cnt.begin(), cnt.begin() + num_of_ranks, 0);
     for (int i = 0; i < n; i++) {
-      cnt[rank[index[i]]]++;
+      cnt[rank[i]]++;
     }
     for (int i = 1; i < num_of_ranks; i++) {
       cnt[i] += cnt[i - 1];
     }
+    for (int i = 0; i < n; i++) {
+      index[i] -= w;
+      if (index[i] < 0) index[i] += n;
+    }
     for (int i = n - 1; i >= 0; i--) {
-      nindex[--cnt[rank[index[i]]]] = index[i];    
+      nindex[--cnt[rank[index[i]]]] = index[i]; // order[index[i]] = --cnt[rank[index[i]]]
     }
 
     num_of_ranks = 1;
     nrank[nindex[0]] = 0;
     for (int i = 1; i < n; i++) {
-      std::pair<int, int> cur = {rank[nindex[i]], rank[(nindex[i] + (1 << step)) % n]};
-      std::pair<int, int> prev = {rank[nindex[i - 1]], rank[(nindex[i - 1] + (1 << step)) % n]};
+      std::pair<int, int> cur = {rank[nindex[i]], rank[(nindex[i] + w) % n]};
+      std::pair<int, int> prev = {rank[nindex[i - 1]], rank[(nindex[i - 1] + w) % n]};
       if (cur != prev) num_of_ranks++;
       nrank[nindex[i]] = num_of_ranks - 1;
     }
