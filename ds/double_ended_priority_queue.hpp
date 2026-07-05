b00f5bef5d000ae1cd4c2b99b7aca70b78447a26
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-05 19:09:18 +0900

    replace tabs with spaces

--- ds/double_ended_priority_queue.hpp
+++ ds/double_ended_priority_queue.hpp
@@ -15,34 +15,34 @@ template <class T> struct double_ended_priority_queue {
 
   T min() const {
     assert(!empty());
-	  return d[2];
+    return d[2];
   }
   T max() const {
     assert(!empty());
-	  return int(d.size()) == 3 ? d[2] : d[3];
+    return int(d.size()) == 3 ? d[2] : d[3];
   }
 
   void push(T x) {
-	  d.push_back(x);
-	  up(int(d.size()) - 1);
+    d.push_back(x);
+    up(int(d.size()) - 1);
   }
 
   void pop_min() {
     assert(!empty());
-	  if (int(d.size()) == 3) {
-	    d.pop_back();
+    if (int(d.size()) == 3) {
+      d.pop_back();
       return;
-	  }
+    }
     std::swap(d[2], d.back());
     d.pop_back();
     up(down(2));
   }
   void pop_max() {
     assert(!empty());
-	  if (int(d.size()) <= 4) {
-	    d.pop_back(); 
+    if (int(d.size()) <= 4) {
+      d.pop_back(); 
       return;
-	  }
+    }
     std::swap(d[3], d.back());
     d.pop_back();
     up(down(3));
@@ -55,7 +55,7 @@ private:
   std::vector<T> d;
 
   int down(int cur) {
-	  int n = int(d.size());
+    int n = int(d.size());
     if (cur == 2) {
       while (true) {
         int nxt = 2 * cur;
@@ -75,15 +75,15 @@ private:
         } else break;
       }
     }
-	  return cur;
+    return cur;
   }
 
   int up(int cur) {
     int n = int(d.size());
-	  if ((cur | 1) < n && d[cur & ~1] > d[cur | 1]) {
-	    std::swap(d[cur & ~1], d[cur | 1]);
-	    cur ^= 1;
-	  }
+    if ((cur | 1) < n && d[cur & ~1] > d[cur | 1]) {
+      std::swap(d[cur & ~1], d[cur | 1]);
+      cur ^= 1;
+    }
     if (d[cur] < d[cur / 4 * 2]) {
       while (true) {
         int nxt = cur / 4 * 2;
