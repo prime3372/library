06f28a0efde785bc4aeafefd4a5d615f6cc561ae
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-08 09:14:38 +0900

    fix

--- random/random_str.hpp
+++ random/random_str.hpp
@@ -1,5 +1,6 @@
 #pragma once  
-  
+
+#include <cassert>
 #include <string>
 
 #include "random/rng.hpp"
@@ -7,6 +8,7 @@
 namespace cp {
 
 template <bool upper = false> std::string random_str(int n) {
+  assert(0 <= n);
   std::string res(n, 0);
   for (int i = 0; i < n; i++) {
     res[i] = char((upper ? 'A' : 'a') + mt32() % 26);
@@ -15,6 +17,7 @@ template <bool upper = false> std::string random_str(int n) {
 }
 
 std::string random_str(int n, std::string s) {
+  assert(0 <= n);
   std::string res(n, 0);
   for (int i = 0; i < n; i++) {
     res[i] = s[mt32() % s.size()];
@@ -23,7 +26,9 @@ std::string random_str(int n, std::string s) {
 }
 
 std::string random_dec(int n) {
-  return random_str(n, "0123456789");
+  assert(0 <= n);
+  if (n == 0) return "";
+  else return random_str(1, "123456789") + random_str(n - 1, "0123456789");
 }
 
 } // namespace cp
