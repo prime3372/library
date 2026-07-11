2c921812c2ea4f89e6845f2bda7e10861541dbaf
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-12 04:19:29 +0900

    refactor

--- number/is_prime.hpp
+++ number/is_prime.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <concepts>
+#include <vector>
 
 #include "number/barrett.hpp"
 #include "number/pow_mod.hpp"
@@ -9,39 +10,17 @@ namespace cp {
 
 // M. Forisek and J. Jancina,
 // Fast Primality Testing for Integers That Fit into a Machine Word
-template <std::integral T> requires(sizeof(T) <= 4)
-constexpr bool is_prime(T n) {
-  if (n <= 2) return n == 2;
-  if (n % 2 == 0) return false;
-  long long d = (long long)(n) - 1;
-  while (d % 2 == 0) d /= 2;
-  barrett bt((unsigned int)(n));
-
-  for (long long a : {2, 7, 61}) {
-    if (a % n == 0) continue;
-    long long y = pow_mod(a, d, n);
-    if (y == 1) continue;
-
-    bool ok = false;
-    for (long long t = d; t != (long long)(n) - 1; t *= 2) {
-      if (y == (long long)(n) - 1) {
-        ok = true;
-        break;
-      }
-      y = bt.mul(y, y);
-    }
-    if (!ok) return false;
-  }
-  return true;
-}
-
 constexpr bool is_prime(long long n) {
   if (n <= 2) return n == 2;
   if (n % 2 == 0) return false; 
   long long d = n - 1;
   while (d % 2 == 0) d /= 2;
 
-  for (long long a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
+  std::vector<long long> bases;
+  if (n < 4759123141) bases = {2, 7, 61};
+  else bases = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+
+  for (long long a : bases) {
     if (a % n == 0) continue;
     __int128 y = pow_mod(a, d, n);
     if (y == 1) continue;
