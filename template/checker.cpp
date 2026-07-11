81fae137467cc7080d49d5dfe32599daf76efa1b
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-12 01:18:44 +0900

    modify

--- template/checker.cpp
+++ template/checker.cpp
@@ -3,20 +3,13 @@
 using namespace std;
 using ll = long long;
 
-#define must(expr) do { \
-                     if (!(expr)) { \
-                       cout << "wrong answer " << #expr << " is not satisfied, line " << __LINE__ << endl; \
-                       exit(1); \
-                     } \
-                   } while (false)
-
-#define mustnt(expr) do { \
-                      if (expr) { \
-                         cout << "wrong answer " << #expr << " is satisfied, line " << __LINE__ << endl; \
+#undef assert
+#define assert(expr) do { \
+                       if (!(expr)) { \
+                         cout << "wrong answer " << #expr << " is not satisfied, line " << __LINE__ << endl; \
                          exit(1); \
                        } \
                      } while (false)
-
 #define cin
 
 ifstream f_in, f_out, f_ans;
@@ -26,12 +19,11 @@ void check() {
 }
 
 int main(int argc, char* argv[]) {
-  assert(argc >= 4);
   f_in.open(argv[1]);
   f_out.open(argv[2]);
   f_ans.open(argv[3]);
   check();
   string dummy;
-  mustnt(f_out >> dummy);
+  assert(f_out >> dummy);
   return 0;
 }
