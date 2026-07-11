fc98345102c9804cb3a740a09a8daa8fec71bf45
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-12 08:53:40 +0900

    delete trailing spaces

--- tree/cartesian_tree.hpp
+++ tree/cartesian_tree.hpp
@@ -8,10 +8,10 @@ namespace cp {
 template <bool root_is_min> struct cartesian_tree {
   int root;
   std::vector<int> left, right, parent;
-  
+
   cartesian_tree() {}
   template <class T> explicit cartesian_tree(const std::vector<T>& a) { build(a); }
-  
+
   template <class T> void build(const std::vector<T>& a) {
     assert(!a.empty());
     int n = int(a.size());
@@ -31,12 +31,12 @@ template <bool root_is_min> struct cartesian_tree {
       left[i] = k;
       st.push_back(i);
     }
-    
+
     for(int i = 0; i < int(st.size()) - 1; i++) {
       right[st[i]] = st[i + 1];
     }
     root = st[0];
-    
+
     parent.resize(n);
     for (int i = 0; i < n; i++) {
       if (left[i] != -1) parent[left[i]] = i;
