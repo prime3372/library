e7179a87e2d4328ca1d83e48334d4fb3bf2f7152
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-26 01:31:15 +0900

    create util/longest_increasing_sequence.hpp

---
+++ util/longest_increasing_sequence.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+namespace cp {
+
+template <bool strong = true, class T>
+int longest_increasing_sequence(const std::vector<T>& a) {
+  int n = int(a.size());
+  std::vector<T> dp(n, std::numeric_limits<T>::max());
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    int j;
+    if constexpr (strong) {
+      j = int(std::lower_bound(dp.begin(), dp.end(), a[i]) - dp.begin());
+    } else {
+      j = int(std::upper_bound(dp.begin(), dp.end(), a[i]) - dp.begin());
+    }
+    dp[j] = a[i];
+    ans = std::max(ans, j + 1);
+  }
+  return ans;
+}
+
+} // namespace cp
