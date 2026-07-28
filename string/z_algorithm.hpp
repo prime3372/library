bc1c4ec368365447afc1251ae8ade3aca8ddb94b
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-28 18:51:29 +0900

    a

--- string/z_algorithm.hpp
+++ string/z_algorithm.hpp
@@ -10,9 +10,13 @@ template <class Str> std::vector<int> z_algorithm(const Str& s) {
   std::vector<int> z(n);
   for (int i = 1, j = 0; i < n; i++) {
     // let s[l, r) denotes s[l,l+1,...,r-1], then
-    // s[j, j+z[j]) = s[0, z[j])
-    // s[i-j, i-j+z[i-j]) = s[0, z[i-j])
-    if (i < j + z[j]) z[i] = std::min(z[i - j], j + z[j] - i); // s[i, j+z[j]) = s[i-j, z[j]) = s[0, min(z[i-j],j+z[j]-i))
+    // [1] s[j, j+z[j]) = s[0, z[j])
+    // [2] s[i-j, i-j+z[i-j]) = s[0, z[i-j])
+
+    if (i < j + z[j]) z[i] = std::min(z[i - j], j + z[j] - i);
+    // s[i, j+z[j]) = s[i-j, z[j])                ...by[1]
+    //              = s[0, min(z[i-j], j+z[j]-i)) ...by[2]
+
     while (i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
     if (j + z[j] < i + z[i]) j = i;
   }
