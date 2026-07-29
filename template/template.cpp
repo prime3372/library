a134cb205ffc8e15b2188d0b344621db106837f0
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-30 01:31:53 +0900

    update template\template.cpp again

--- template/template.cpp
+++ template/template.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
+using namespace std;
 #include "util/int128.hpp"
 #include "util/static_modint.hpp"
-using namespace std;
 using namespace cp;
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
 #define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
 #define all(a) a.begin(), a.end()
 using ll = long long;
+template <class T> using vec = std::vector<T>;
 template <class T> bool chmin(T& a, T b) { if (b < a) { a = b; return 1; } else return 0; }
-template <class T> bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } else return 0; }
+template <class T> bool chmax(T& a, T b) { if (b > a) { a = b; return 1; } else return 0; }
 
 int main() {
   ios_base::sync_with_stdio(false);
