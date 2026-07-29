a997c91949872e6d520c622caaad79ec93905b44
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-30 00:49:06 +0900

    update template\template.cpp

--- template/template.cpp
+++ template/template.cpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
+#include "util/int128.hpp"
+#include "util/static_modint.hpp"
+using namespace std;
+using namespace cp;
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
 #define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
 #define all(a) a.begin(), a.end()
-using namespace std;
 using ll = long long;
 template <class T> bool chmin(T& a, T b) { if (b < a) { a = b; return 1; } else return 0; }
 template <class T> bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } else return 0; }
