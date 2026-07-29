94ef964ea6e756df40f63b2f7899253bd366739f
prime3372 <syunrurusera@yahoo.ne.jp>
2026-07-30 03:03:44 +0900

    a

--- template/template.cpp
+++ template/template.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "util/ios_overload.hpp"
-#include "util/static_modint.hpp"
-using namespace cp;
 #define rep(i, a, b) for (int i = int(a); i < int(b); i++)
 #define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
 #define all(a) a.begin(), a.end()
 using ll = long long;
-using mint = modint998244353;
 template <class T> using vec = vector<T>;
 template <class T> bool chmin(T& a, T b) { if (b < a) { a = b; return 1; } else return 0; }
 template <class T> bool chmax(T& a, T b) { if (b > a) { a = b; return 1; } else return 0; }
+#include "util/ios_overload.hpp"
+#include "util/static_modint.hpp"
+using namespace cp;
+using mint = modint998244353;
 
 int main() {
   ios_base::sync_with_stdio(false);
