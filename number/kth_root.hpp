eae6a325021a73c95e3cd8554259b990e08c45db
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-01 20:08:53 +0900

    improve kth_root implementation

--- number/kth_root.hpp
+++ number/kth_root.hpp
@@ -3,26 +3,35 @@
 #include <cassert>
 #include <cmath>
 
+#include "util/math_utility.hpp"
 #include "util/type_traits.hpp"
 
 namespace cp {
 
-template <class T, class U>
-constexpr T kth_root(T x, U k) {
+template <class T>
+  requires internal::is_integral_v<T>
+constexpr T kth_root(T x, int k) {
   assert(0 <= x && 1 <= k);
   if (x <= 1 || k == 1) return x;
-  if (64 <= k) return 1;
-  auto check = [&](unsigned __int128 y) {
-    unsigned __int128 z = 1;
+  if (k == 2) return isqrt(x);
+  if (k == 3) return icbrt(x);
+
+  auto is_small_enough = [&](T y) {
+    if (y == 0) return true;
+    T z = 1;
     for (int e = k; e; e >>= 1) {
-      if (e & 1) z *= y;
-      y *= y;
+      if (e & 1) z *= y;      
+      if (e > 1) {
+        if (z > x / y / y) return false;
+        y *= y;
+      }
     }
-    return z <= (unsigned __int128)(x);
+    return z <= x;
   };
+
   T y = T(std::pow(double(x), 1.0 / k));
-  while (!check(y)) y--;
-  while (check(y + 1)) y++;
+  while (!is_small_enough(y)) y--;
+  while (is_small_enough(y + 1)) y++;
   return y;
 }
 
