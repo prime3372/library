f86982fdfd5becb5121f7f524be89df510a30642
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-09 07:08:10 +0900

    create test\yosupo\rooted_tree_isomorphism_classification.cpp

---
+++ test/yosupo/rooted_tree_isomorphism_classification.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/rooted_tree_isomorphism_classification"
+
+#include "tree/rooted_tree_hash.hpp"
+
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = int(a); i < int(b); i++)
+using namespace std;
+using namespace cp;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  rooted_tree_hash t(n);
+  rep(i, 1, n) {
+    int p;
+    cin >> p;
+    t.add_edge(i, p);
+  }
+  auto hs = t.build(0).hash;
+  vector<pair<hash61, int>> hs_idx(n);
+  rep(i, 0, n) hs_idx[i] = {hs[i], i};
+  sort(hs_idx.begin(), hs_idx.end());
+  int id = 0;
+  vector<int> ans(n);
+  for (int i = 0; i < n; i++) {
+    ans[hs_idx[i].second] = id;
+    if (i < n - 1 && hs_idx[i].first != hs_idx[i + 1].first) id++;
+  }
+  cout << id + 1 << "\n";
+  rep(i, 0, n) cout << ans[i] << " ";
+}
---
+++ tree/rooted_tree_hash.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+#include "util/hash61.hpp"
+
+namespace cp {
+
+struct rooted_tree_hash {
+public:
+  rooted_tree_hash(int _n) : hash(_n), depth(_n), n(_n), g(_n) {
+    while (int(xs.size()) <= n) {
+      xs.push_back(hash61::get_rand());
+    }
+  }
+
+  void add_edge(int a, int b) {
+    assert(0 <= a && a < n);
+    assert(0 <= b && b < n);
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+
+  std::vector<hash61> hash;
+  std::vector<int> depth;
+
+  rooted_tree_hash& build(int r) {
+    dfs(r, -1);
+    return *this;
+  }
+
+private:
+  inline static std::vector<hash61> xs;
+  int n;
+  std::vector<std::vector<int>> g;
+
+ int dfs(int v, int pv) {
+    int dep = 0;
+    for (int nv : g[v]) {
+      if (nv != pv) dep = std::max(dep, dfs(nv, v) + 1);
+    }
+    hash61 x = xs[dep], h = 1;
+    for (int nv : g[v]) {
+      if (nv != pv) h = h * (x + hash[nv]);
+    }
+    hash[v] = h;
+    return depth[v] = dep;
+  }
+};
+
+} // namespace cp
