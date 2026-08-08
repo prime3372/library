b92bc74d707fd49964b14c84782ac0ead22e50cf
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-08 19:14:29 +0900

    separate primitive_root_constexpr.hpp from convolution.hpp

--- poly/convolution.hpp
+++ poly/convolution.hpp
@@ -6,38 +6,13 @@
 
 #include "number/is_prime.hpp"
 #include "number/pow_mod.hpp"
+#include "number/primitive_root_constexpr.hpp"
 #include "util/type_traits.hpp"
 #include "util/static_modint.hpp"
 
 namespace cp {
 
 namespace internal {
-
-constexpr int primitive_root_constexpr(int p) {
-  if (p == 2) return 1;
-
-  std::vector<int> fac = {2};
-  int x = p - 1;
-  while (x % 2 == 0) x /= 2;
-  for (int i = 3; 1LL * i * i <= x; i += 2) {
-    if (x % i == 0) {
-      fac.push_back(i);
-      while (x % i == 0) x /= i;
-    }
-  }
-  if (x > 1) fac.push_back(x);
-
-  for (int g = 2;; g++) {
-    bool ok = true;
-    for (int f : fac) {
-      if (pow_mod(g, (p - 1) / f, p) == 1) {
-        ok = false;
-        break;
-      }
-    }
-    if (ok) return g;
-  }
-}
 
 template <class mint, int g> std::vector<mint> ntt_root() {
   constexpr int rank2 = std::countr_zero((unsigned int)(mint::mod() - 1));
