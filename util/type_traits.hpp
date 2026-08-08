fd984b1a7a5b125e95b681601303b4504afa1a18
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-09 07:23:19 +0900

    a

--- util/type_traits.hpp
+++ util/type_traits.hpp
@@ -57,35 +57,17 @@ using is_unsigned_int =
 template <class T>
 inline constexpr bool is_unsigned_int_v = is_unsigned_int<T>::value;
 
-// copy_cv
-
-template <class T, class U>
-using copy_cv =
-  std::conditional<
-    std::is_const_v<T>,
-    std::conditional_t<
-      std::is_volatile_v<T>,
-      const volatile U,
-      const T>,
-    std::conditional_t<
-      std::is_volatile_v<T>,
-      volatile U,
-      U>>;
-
-template <class T, class U>
-using copy_cv_t = typename copy_cv<T, U>::type;
-
-// make_unsigned
+// to_unsigned
 
 template <class T>
-using make_unsigned =
+using to_unsigned =
   std::conditional<
     is_signed_int128_v<T> || is_unsigned_int128_v<T>,
-    copy_cv_t<T, unsigned __int128>,
-    std::make_unsigned_t<T>>;
+    unsigned __int128,
+    std::remove_cv_t<std::make_unsigned_t<T>>>;
 
 template <class T>
-using make_unsigned_t = typename make_unsigned<T>::type;
+using to_unsigned_t = typename to_unsigned<T>::type;
 
 // is_tuple_like
 
