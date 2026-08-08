dbce0d7d1009452c307b56e31419f1315d867eb7
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-09 04:53:04 +0900

    create algebra\arithmetic_add.hpp

---
+++ algebra/arithmetic_add.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstddef>
+
+namespace cp {
+
+template <class T> struct arithmetic_add {
+  struct S {
+    T val_sum;
+    size_t index_sum, len;
+  };
+  static S op(S x, S y) {
+    return S{x.val_sum + y.val_sum, x.index_sum + y.index_sum, x.len + y.len};
+  }
+  static S e() { return S{0, 0, 0}; }
+
+  struct F {
+    T a, b;
+  };
+  static S mapping(F f, S x) {
+    return S{x.val_sum + f.a * x.index_sum + f.b * x.len, x.index_sum, x.len};
+  }
+  static F composition(F g, F f) { return F{f.a + g.a, f.b + g.b}; }
+  static F id() { return F{0, 0}; }
+};
+
+} // namespace cp
