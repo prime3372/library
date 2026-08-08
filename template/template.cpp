d7ccf0f17ba13ad7471e77dceb8f4d6e30d78810
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-09 01:52:05 +0900

    add alias to template.cpp

--- template/template.cpp
+++ template/template.cpp
@@ -18,6 +18,7 @@ using i128 = __int128;
 using u128 = unsigned __int128;
 using mint = modint998244353;
 template <class T> using vec = vector<T>;
+template <class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
 
 int main() {
   ios_base::sync_with_stdio(false);
