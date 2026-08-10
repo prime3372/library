b9e68ec44e22e35dc69ee0d6bcecc541c8052e9a
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-10 12:42:59 +0900

    a

--- util/hash_utility.hpp
+++ util/hash_utility.hpp
@@ -17,7 +17,7 @@ namespace internal {
 
 template <class T> struct hash {};
 
-template <class T> requires internal::is_integral_v<T>
+template <class T> requires is_integral_v<T>
 struct hash<T> {
   unsigned long long operator()(const T& x) const {
     static const unsigned long long fixed_random = mt64();
@@ -42,19 +42,19 @@ template <class T> void hash_combine(unsigned long long& seed, const T& val) {
 template <> struct hash<std::string> {
   unsigned long long operator()(const std::string& s) const {
     unsigned long long hs = 0;
-    for (auto& c : s) internal::hash_combine(hs, c);
+    for (auto& c : s) hash_combine(hs, c);
     return hs;
   }
 };
 
 // tuple_like (array, pair, tuple)
 
-template <class Tuple> requires internal::is_tuple_like_v<Tuple>
+template <class Tuple> requires is_tuple_like_v<Tuple>
 struct hash<Tuple> {
   unsigned long long operator()(const Tuple& t) const {
     unsigned long long hs = 0;
     [&]<size_t... I>(std::index_sequence<I...>) {
-      (internal::hash_combine(hs, std::get<I>(t)), ...);
+      (hash_combine(hs, std::get<I>(t)), ...);
     }(std::make_index_sequence<std::tuple_size_v<Tuple>>());
     return hs;
   }
@@ -65,7 +65,7 @@ struct hash<Tuple> {
 template <class T, class Alloc> struct hash<std::vector<T, Alloc>> {
   unsigned long long operator()(const std::vector<T, Alloc>& v) const {
     unsigned long long hs = 0;
-    for (const auto& x : v) internal::hash_combine(hs, x);
+    for (const auto& x : v) hash_combine(hs, x);
     return hs;
   }
 };
