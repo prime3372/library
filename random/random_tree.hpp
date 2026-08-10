18acc666f7f738379992387ea27a4cbece9a7348
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-10 20:34:38 +0900

    create random_rooted_tree

--- random/random_tree.hpp
+++ random/random_tree.hpp
@@ -9,7 +9,7 @@
 
 namespace cp {
 
-std::vector<std::pair<int, int>> random_tree(int n, int root = 0) {
+std::vector<int> random_rooted_tree(int n, int root = 0) {
   assert(1 <= n);
   assert(0 <= root && root < n);
   if (n == 1) return {};
@@ -22,14 +22,25 @@ std::vector<std::pair<int, int>> random_tree(int n, int root = 0) {
     par[i] = uniform(root, n + i - 1) % n;
   }
 
+  return par;
+}
+
+std::vector<std::pair<int, int>> random_tree(int n) {
+  assert(1 <= n);
+
+  auto par = random_rooted_tree(n, uniform(0, n - 1));
+
   std::vector<std::pair<int, int>> edges;
   edges.reserve(n - 1);
   for (int i = 0; i < n; i++) {
     if (par[i] == -1) continue;
     edges.emplace_back(par[i], i);
   }
-  std::shuffle(edges.begin(), edges.end(), mt32);
 
+  std::shuffle(edges.begin(), edges.end(), mt32);
+  for (int i = 0; i < n - 1; i++) {
+    if (uniform_bool()) std::swap(edges[i].first, edges[i].second);
+  }
   return edges;
 }
 
