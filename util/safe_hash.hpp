a7cd9ca4586f9e2fa26eb8c9b2efc4739c18081a
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-12 23:34:50 +0900

    fix safe_hash.hpp

--- util/safe_hash.hpp
+++ util/safe_hash.hpp
@@ -13,75 +13,79 @@
 
 namespace cp {
 
-template <class T> struct safe_hash {};
+template <class> struct safe_hash {};
+
+namespace internal {
+
+static unsigned long long splitmix64(unsigned long long x) {
+  static const unsigned long long fixed_rand = mt64();
+  x += fixed_rand;
+  x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+  x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+  return x ^ (x >> 31);
+}
+
+template <class T> void hash_combine(unsigned long long& seed, const T& val) {
+  seed = internal::splitmix64(seed) ^ (safe_hash<T>()(val) + 0x9e3779b9 + (seed << 6) + (seed >> 2));
+}
+
+} // namespace internal
 
 template <class T> requires internal::is_integral_v<T> && (sizeof(T) <= 8)
 struct safe_hash<T> {
   unsigned long long operator()(const T& x) const {
-    static const unsigned long long fixed_random = mt64();
-    unsigned long long hs = (unsigned long long)(x);
-    hs += fixed_random;
-    hs = (hs ^ (hs >> 30)) * 0xbf58476d1ce4e5b9;
-    hs = (hs ^ (hs >> 27)) * 0x94d049bb133111eb;;
-    return hs ^ (hs >> 31);
+    return internal::splitmix64((unsigned long long)(x));
   }
 };
 
-template <class T> void hash_combine(unsigned long long& seed, const T& val) {
-  static const unsigned long long fixed_random = mt64();
-  seed += fixed_random;
-  seed ^= safe_hash<T>()(val);
-  seed = (seed ^ (seed >> 30)) * 0xbf58476d1ce4e5b9;
-}
-
 template <> struct safe_hash<__int128> {
   unsigned long long operator()(const __int128& x) const {
+    if ((x >> 64) == 0) {
+      return safe_hash<unsigned long long>()((unsigned long long)(x));
+    }
     unsigned long long hs = 0;
-    hash_combine(hs, (unsigned long long)((unsigned __int128)(x) >> 64));
-    hash_combine(hs, (unsigned long long)(x));
+    internal::hash_combine(hs, (unsigned long long)((unsigned __int128)(x) >> 64));
+    internal::hash_combine(hs, (unsigned long long)(x));
     return hs;
   }
 };
 
 template <> struct safe_hash<unsigned __int128> {
   unsigned long long operator()(const unsigned __int128& x) const {
+    if ((x >> 64) == 0) {
+      return safe_hash<unsigned long long>()((unsigned long long)(x));
+    }
     unsigned long long hs = 0;
-    hash_combine(hs, (unsigned long long)(x >> 64));
-    hash_combine(hs, (unsigned long long)(x));
+    internal::hash_combine(hs, (unsigned long long)(x >> 64));
+    internal::hash_combine(hs, (unsigned long long)(x));
     return hs;
   }
 };
-
-// string
 
 template <> struct safe_hash<std::string> {
   unsigned long long operator()(const std::string& s) const {
     unsigned long long hs = 0;
-    for (auto& c : s) hash_combine(hs, c);
+    for (auto& c : s) internal::hash_combine(hs, c);
     return hs;
   }
 };
-
-// tuple_like (array, pair, tuple)
 
 template <class Tuple> requires internal::is_tuple_like_v<Tuple>
 struct safe_hash<Tuple> {
   unsigned long long operator()(const Tuple& t) const {
     unsigned long long hs = 0;
     [&]<size_t... I>(std::index_sequence<I...>) {
-      (hash_combine(hs, std::get<I>(t)), ...);
+      (internal::hash_combine(hs, std::get<I>(t)), ...);
     }(std::make_index_sequence<std::tuple_size_v<Tuple>>());
     return hs;
   }
 };
 
-// vector
-
 template <class T, class Alloc>
 struct safe_hash<std::vector<T, Alloc>> {
   unsigned long long operator()(const std::vector<T, Alloc>& v) const {
     unsigned long long hs = 0;
-    for (const auto& x : v) hash_combine(hs, x);
+    for (const auto& x : v) internal::hash_combine(hs, x);
     return hs;
   }
 };
