d4b05fe3f55885220bb2d8bc0ea9120bc8ea9618
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-13 06:43:06 +0900

    renew template

---
+++ template/macros.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#define rep(i, a, b)   for (int i = (a); i < int(b); i++)
+#define REP(i, a, b)   for (int i = (a); i <= int(b); i++)
+#define rrep(i, a, b)  for (int i = int(b) - 1; i >= (a); i--)
+#define RREP(i, a, b)  for (int i = int(b); i >= (a); i--)
+#define fore(x, a)     for (auto& x : (a))
+#define elif           else if
+#define all(a)         (a).begin(), (a).end()
+
+#ifdef LOCAL
+
+#define EMPTY
+#define DEFER(m) m EMPTY
+
+#define EVAL(...)  EVAL1(EVAL1(__VA_ARGS__))
+#define EVAL1(...) EVAL2(EVAL2(__VA_ARGS__))
+#define EVAL2(...) EVAL3(EVAL3(__VA_ARGS__))
+#define EVAL3(...) EVAL4(EVAL4(__VA_ARGS__))
+#define EVAL4(...) EVAL5(EVAL5(__VA_ARGS__))
+#define EVAL5(...) EVAL6(EVAL6(__VA_ARGS__))
+#define EVAL6(...) __VA_ARGS__
+
+#define INVOKE_RECURSIVE_INDIRECT() INVOKE_RECURSIVE
+
+#define INVOKE_RECURSIVE(macro, first, ...) \
+  do { \
+    macro(first); \
+    __VA_OPT__(DEFER(INVOKE_RECURSIVE_INDIRECT)()(macro, __VA_ARGS__)); \
+  } while (false)
+
+#define INVOKE(macro, ...) EVAL(INVOKE_RECURSIVE(macro, __VA_ARGS__))
+
+#define DEBUG(a) do { std::cerr << (a) << " "; } while (false)
+
+#define debug(...) do { INVOKE(DEBUG, __VA_ARGS__); std::cerr << "\n"; } while (false)
+
+#else
+
+#define debug(...)
+
+#endif
--- template/template.cpp
+++ template/template.cpp
@@ -2,15 +2,7 @@
 #include "util/math_utility.hpp"
 #include "util/static_modint.hpp"
 
-#pragma region
-
 #include <bits/stdc++.h>
-#define rep(i, a, b) for (int i = (a); i < int(b); i++)
-#define REP(i, a, b) for (int i = (a); i <= int(b); i++)
-#define rrep(i, a, b) for (int i = int(b) - 1; i >= (a); i--)
-#define RREP(i, a, b) for (int i = int(b); i >= (a); i--)
-#define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
 using namespace std;
 using namespace cp;
 using uint = unsigned int;
@@ -22,7 +14,7 @@ using mint = modint998244353;
 template <class T> using vec = vector<T>;
 template <class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
 
-#pragma endregion
+#include "template/macros.hpp"
 
 int main() {
   ios_base::sync_with_stdio(false);
