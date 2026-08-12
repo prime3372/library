826628777c73458f808bacb8f9c495f6bde36d4b
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-13 07:20:25 +0900

    improve hash_set

--- ds/hash_set.hpp
+++ ds/hash_set.hpp
@@ -1,13 +1,30 @@
 #pragma once
 
+#include <vector>
+
 #include "ds/hash_map.hpp"
 
 namespace cp {
 
 template <class T> struct hash_set {
 public:
+  hash_set() {}
+
   void insert(const T& x) { mp[x]; }
+
   bool count(const T& x) const { return mp.count(x); }
+
+  std::vector<T> enumerate() const {
+    auto pairs = mp.enumerate();
+    std::vector<T> res(e.size());
+    for (int i = 0; i < int(pairs.size()); i++) {
+      res[i] = pairs[i].first;
+    }
+    return res;
+  }
+
+  int size() const { return mp.size(); }
+
 private:
   hash_map<T, char> mp;
 };
