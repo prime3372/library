d70e4391e4662b008a82dafcc6ffb6e776768b51
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-13 05:09:49 +0900

    create test\aoj\DSL_3_D_sliding_minimum_elements.test.cpp

---
+++ test/aoj/DSL_3_D_sliding_minimum_elements.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_3_D"
+
+#include "algebra/min.hpp"
+#include "ds/slide_window_aggregation.hpp"
+
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = int(a); i < int(b); i++)
+using namespace std;
+using namespace cp;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, l;
+  cin >> n >> l;
+  slide_window_aggregation<alg::min<int, int(1e9) + 1>> swag;
+  rep(i, 0, l) {
+    int a;
+    cin >> a;
+    swag.push(a);
+  }
+  cout << swag.prod();
+  if (l != n) cout << " ";
+  rep(i, 0, n - l) {
+    int a;
+    cin >> a;
+    swag.push(a);
+    swag.pop();
+    cout << swag.prod();
+    if (i != n - l - 1) cout << " ";
+  }
+  cout << "\n";
+}
