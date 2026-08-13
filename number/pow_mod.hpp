e8d8c01834b058ea1cbad2cb913a3984ad001be1
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-14 00:05:25 +0900

    a

--- number/pow_mod.hpp
+++ number/pow_mod.hpp
@@ -24,8 +24,8 @@ constexpr long long pow_mod(long long x, long long n, long long m) {
     __int128 r = 1, y = x % m;
     if (y < 0) y += m;
     while (n) {
-      if (n & 1) r = r * y % m;
-      y = y * y % m;
+      if (n & 1) r = (r * y) % m;
+      y = (y * y) % m;
       n >>= 1;
     }
     return (long long)(r);
