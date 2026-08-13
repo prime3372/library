19b28e4161f6b429ec5f7aa7c0a12494a67a5026
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-14 08:39:05 +0900

    refactor safe_hash

--- util/safe_hash.hpp
+++ util/safe_hash.hpp
@@ -2,9 +2,8 @@
 
 #include <array>
 #include <cstddef>
-#include <string>
+#include <ranges>
 #include <tuple>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -52,30 +51,22 @@ struct safe_hash<T> {
   }
 };
 
-template <> struct safe_hash<std::string> {
-  unsigned long long operator()(const std::string& s) const {
+template <class Range> requires std::ranges::range<Range>
+struct safe_hash<Range> {
+  unsigned long long operator()(const Range& a) const {
     unsigned long long hs = 0;
-    for (auto& c : s) internal::hash_combine(hs, c);
+    for (const auto& x : a) internal::hash_combine(hs, x);
     return hs;
   }
 };
 
-template <class Tuple> requires internal::is_tuple_like_v<Tuple>
+template <class Tuple> requires internal::is_tuple_v<Tuple>
 struct safe_hash<Tuple> {
   unsigned long long operator()(const Tuple& t) const {
     unsigned long long hs = 0;
     [&]<size_t... I>(std::index_sequence<I...>) {
       (internal::hash_combine(hs, std::get<I>(t)), ...);
     }(std::make_index_sequence<std::tuple_size_v<Tuple>>());
-    return hs;
-  }
-};
-
-template <class T, class Alloc>
-struct safe_hash<std::vector<T, Alloc>> {
-  unsigned long long operator()(const std::vector<T, Alloc>& v) const {
-    unsigned long long hs = 0;
-    for (const auto& x : v) internal::hash_combine(hs, x);
     return hs;
   }
 };
