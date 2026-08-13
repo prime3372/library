d7406470d2484c4663ed116b820773df49405541
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-13 10:04:25 +0900

    simplify debug macro

--- template/macros.hpp
+++ template/macros.hpp
@@ -9,30 +9,11 @@
 
 #ifdef LOCAL
 
-#define EMPTY
-#define DEFER(m) m EMPTY
-
-#define EVAL(...)  EVAL1(EVAL1(__VA_ARGS__))
-#define EVAL1(...) EVAL2(EVAL2(__VA_ARGS__))
-#define EVAL2(...) EVAL3(EVAL3(__VA_ARGS__))
-#define EVAL3(...) EVAL4(EVAL4(__VA_ARGS__))
-#define EVAL4(...) EVAL5(EVAL5(__VA_ARGS__))
-#define EVAL5(...) EVAL6(EVAL6(__VA_ARGS__))
-#define EVAL6(...) __VA_ARGS__
-
-#define INVOKE_RECURSIVE_INDIRECT() INVOKE_RECURSIVE
-
-#define INVOKE_RECURSIVE(macro, first, ...) \
-  do { \
-    macro(first); \
-    __VA_OPT__(DEFER(INVOKE_RECURSIVE_INDIRECT)()(macro, __VA_ARGS__)); \
-  } while (false)
-
-#define INVOKE(macro, ...) EVAL(INVOKE_RECURSIVE(macro, __VA_ARGS__))
-
-#define DEBUG(a) do { std::cerr << (a); } while (false)
-
-#define debug(...) do { INVOKE(DEBUG, __VA_ARGS__); std::cerr << "\n"; } while (false)
+#define debug(...) \
+  []<class... Args>(Args... args) -> void { \
+    (std::cerr << ... << args); \
+    std::cerr << "\n"; \
+  }(__VA_ARGS__)
 
 #else
 
