ca7c65e823fb5f076599119408087772b821f400
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-14 16:42:43 +0900

    refactor template

--- template/template.cpp
+++ template/template.cpp
@@ -1,20 +1,4 @@
-#include "util/io_utility.hpp"
-#include "util/math_utility.hpp"
-#include "util/static_modint.hpp"
-
-#include <bits/stdc++.h>
-using namespace std;
-using namespace cp;
-using uint = unsigned int;
-using ll = long long;
-using ull = unsigned long long;
-using i128 = __int128;
-using u128 = unsigned __int128;
-using mint = modint998244353;
-template <class T> using vec = vector<T>;
-template <class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
-
-#include "template/macros.hpp"
+#include "template/template.hpp"
 
 int main() {
   ios_base::sync_with_stdio(false);
--- template/template.hpp
+++ template/template.hpp
@@ -1,5 +1,21 @@
 #pragma once
 
+#include "util/io_utility.hpp"
+#include "util/math_utility.hpp"
+#include "util/static_modint.hpp"
+
+#include <bits/stdc++.h>
+using namespace std;
+using namespace cp;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+using u128 = unsigned __int128;
+using mint = modint998244353;
+template <class T> using vec = vector<T>;
+template <class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
+
 #define rep(i, a, b)   for (int i = (a); i < int(b); i++)
 #define REP(i, a, b)   for (int i = (a); i <= int(b); i++)
 #define rrep(i, a, b)  for (int i = int(b) - 1; i >= (a); i--)
