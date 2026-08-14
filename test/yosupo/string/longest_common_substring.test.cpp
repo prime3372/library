3b5471fd0b483a6d4486430584518c591ef800fa
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-14 23:50:22 +0900

    create longest_common_substring.test.cpp

---
+++ test/yosupo/string/longest_common_substring.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/longest_common_substring"
+
+#include "string/suffix_array.hpp"
+#include "string/lcp_array.hpp"
+
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = (a); i < int(b); i++)
+using namespace std;
+using namespace cp;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+  int n = int(s.size()), m = int(t.size());
+  s.push_back('.');
+  s += t;
+  auto sa = suffix_array(s);
+  auto lcp = lcp_array(s, sa);
+  int max = 0, idx = -1;
+  rep(i, 0, n + m + 1) {
+    bool different = (sa[i] < n) != (sa[i + 1] < n);
+    if (different && max < lcp[i]) {
+      max = lcp[i];
+      idx = i;
+    }
+  }
+  if (max == 0) {
+    cout << 0 << " " << 0 << " " << 0 << " " << 0 << "\n";
+  } else if (sa[idx] < n) {
+    cout << sa[idx] << " " << sa[idx] + max << " ";
+    cout << sa[idx + 1] - n - 1 << " " << sa[idx + 1] + max - n - 1 << "\n";
+  } else {
+    cout << sa[idx + 1] << " " << sa[idx + 1] + max << " ";
+    cout << sa[idx] - n - 1 << " " << sa[idx] + max - n - 1 << "\n";
+  }
+}
