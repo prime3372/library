c9b4d5f65d0b08625bacd479ceaaac0a6d12b2aa
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-14 17:15:51 +0900

    a

--- template/template.hpp
+++ template/template.hpp
@@ -1,11 +1,5 @@
 #pragma once
 
-#include "util/io_utility.hpp"
-#include "util/math_utility.hpp"
-#include "util/static_modint.hpp"
-using namespace cp;
-using mint = modint998244353;
-
 #include <bits/stdc++.h>
 using namespace std;
 using uint = unsigned int;
@@ -15,6 +9,12 @@ using i128 = __int128;
 using u128 = unsigned __int128;
 template <class T> using vec = vector<T>;
 template <class T> using pqueue = priority_queue<T, vector<T>, greater<T>>;
+
+#include "util/io_utility.hpp"
+#include "util/math_utility.hpp"
+#include "util/static_modint.hpp"
+using namespace cp;
+using mint = modint998244353;
 
 #define rep(i, a, b) for (int i = (a); i < int(b); i++)
 #define REP(i, a, b) for (int i = (a); i <= int(b); i++)
