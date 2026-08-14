8d16d7d784f2c4975a3d938830cb41768e63ed8e
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-14 16:44:18 +0900

    a

--- template/template.hpp
+++ template/template.hpp
@@ -16,12 +16,12 @@ using mint = modint998244353;
 template <class T> using vec = vector<T>;
 template <class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
 
-#define rep(i, a, b)   for (int i = (a); i < int(b); i++)
-#define REP(i, a, b)   for (int i = (a); i <= int(b); i++)
-#define rrep(i, a, b)  for (int i = int(b) - 1; i >= (a); i--)
-#define RREP(i, a, b)  for (int i = int(b); i >= (a); i--)
-#define fore(x, a)     for (auto& x : (a))
-#define all(a)         (a).begin(), (a).end()
+#define rep(i, a, b) for (int i = (a); i < int(b); i++)
+#define REP(i, a, b) for (int i = (a); i <= int(b); i++)
+#define rrep(i, a, b) for (int i = int(b) - 1; i >= (a); i--)
+#define RREP(i, a, b) for (int i = int(b); i >= (a); i--)
+#define fore(x, a) for (auto& x : (a))
+#define all(a) (a).begin(), (a).end()
 
 #ifdef LOCAL
 
