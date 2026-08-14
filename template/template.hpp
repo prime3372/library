b62480e3fc92f7afcfdf381567440c0be34bb10f
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-14 16:45:28 +0900

    a

--- template/template.hpp
+++ template/template.hpp
@@ -3,16 +3,16 @@
 #include "util/io_utility.hpp"
 #include "util/math_utility.hpp"
 #include "util/static_modint.hpp"
+using namespace cp;
+using mint = modint998244353;
 
 #include <bits/stdc++.h>
 using namespace std;
-using namespace cp;
 using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
 using i128 = __int128;
 using u128 = unsigned __int128;
-using mint = modint998244353;
 template <class T> using vec = vector<T>;
 template <class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
 
