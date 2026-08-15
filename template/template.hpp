516b2bdbc6944f3ca9f538c474adf36d4e7dce5a
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-15 19:25:07 +0900

    a

--- template/template.hpp
+++ template/template.hpp
@@ -9,7 +9,7 @@ using i128 = __int128;
 using u128 = unsigned __int128;
 template <class T> using vec = vector<T>;
 using str = string;
-template <class T> using pqueue = priority_queue<T, vector<T>, greater<T>>;
+template <class T> using pque = priority_queue<T, vector<T>, greater<T>>;
 
 #include "util/io_utility.hpp"
 #include "util/math_utility.hpp"
