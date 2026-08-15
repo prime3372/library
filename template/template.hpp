187930ac5ebba1435ab6cca5e29183b7d8966985
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-16 02:51:43 +0900

    a

--- template/template.hpp
+++ template/template.hpp
@@ -1,11 +1,5 @@
 #pragma once
 
-#include "util/io_utility.hpp"
-#include "util/math_utility.hpp"
-#include "util/static_modint.hpp"
-using namespace cp;
-using mint = modint998244353;
-
 #include <bits/stdc++.h>
 using namespace std;
 using uint = unsigned int;
@@ -16,14 +10,6 @@ using u128 = unsigned __int128;
 template <class T> using vec = vector<T>;
 using str = string;
 template <class T> using pque = priority_queue<T, vector<T>, greater<T>>;
-
-#define rep(i, a, b) for (int i = (a); i < int(b); i++)
-#define REP(i, a, b) for (int i = (a); i <= int(b); i++)
-#define rrep(i, a, b) for (int i = int(b) - 1; i >= (a); i--)
-#define RREP(i, a, b) for (int i = int(b); i >= (a); i--)
-#define fore(x, a) for (auto& x : (a))
-#define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
 
 #ifdef LOCAL
 
@@ -42,3 +28,17 @@ template <class T> using pque = priority_queue<T, vector<T>, greater<T>>;
 #define debug(...)
 
 #endif
+
+#define rep(i, a, b) for (int i = (a); i < int(b); i++)
+#define REP(i, a, b) for (int i = (a); i <= int(b); i++)
+#define rrep(i, a, b) for (int i = int(b) - 1; i >= (a); i--)
+#define RREP(i, a, b) for (int i = int(b); i >= (a); i--)
+#define fore(x, a) for (auto& x : (a))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+#include "util/io_utility.hpp"
+#include "util/math_utility.hpp"
+#include "util/static_modint.hpp"
+using namespace cp;
+using mint = modint998244353;
