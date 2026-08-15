76d0ddd99d21028643cb69ecbbbffe92d5da2409
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-16 04:41:11 +0900

    create a test for `dijkstra`

---
+++ test/aoj/GRL/1_A_single_sourse_shortest_path.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/GRL/all/GRL_1_A"
+
+#include "graph/dijkstra.hpp"
+
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = (a); i < int(b); i++)
+using namespace std;
+using namespace cp;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m, r;
+  cin >> n >> m >> r;
+  dijkstra<int> g(n);
+  rep(i, 0, m) {
+    int u, v, d;
+    cin >> u >> v >> d;
+    g.add_edge(u, v, d);
+  }
+  g.search(r);
+  rep(i, 0, n) {
+    int ans = g.dist(i);
+    if (ans == -1) cout << "INF\n";
+    else cout << ans << "\n";
+  }
+}
