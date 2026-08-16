b22392f6cd0e49cb422a0744ff2aca3870dd2f9f
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-16 17:38:43 +0900

    improve sort utilities

--- util/algo_utility.hpp
+++ util/algo_utility.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <cstddef>
 #include <numeric>
 #include <utility>
 #include <vector>
@@ -20,27 +21,78 @@ template <class T> void uniq(T& a) {
   a.erase(std::unique(a.begin(), a.end()), a.end());
 }
 
-template <bool descending = false, class... Args>
-std::vector<int> zip_sort(Args&... args) {
-  if (sizeof...(Args) == 0) return {};
-
-  int n = int(std::get<0>(std::tie(args...)).size());
-  assert(((int(args.size()) == n) && ...));
-
-  std::vector<int> p(n);
+template <class Container>
+std::vector<int> sort(Container& a) {
+  std::vector<int> p(int(a.size()));
   std::iota(p.begin(), p.end(), 0);
   std::sort(p.begin(), p.end(), [&](int i, int j) {
-    if (descending) return std::tie(args[i]...) > std::tie(args[j]...);
-    else return std::tie(args[i]...) < std::tie(args[j]...);
+    return a[i] < a[j];
   });
-
-  ([&](auto& arg) {
-    auto tmp = arg;
-    for (int i = 0; i < n; i++) {
-      arg[i] = std::move(tmp[p[i]]);
-    }
-  }(args), ...);
+  auto tmp = a;
+  for (int i = 0; i < int(a.size()); i++) {
+    a[i] = std::move(tmp[p[i]]);
+  }
   return p;
+}
+
+template <class Container, class Comp>
+std::vector<int> sort(Container& a, Comp comp) {
+  std::vector<int> p(int(a.size()));
+  std::iota(p.begin(), p.end(), 0);
+  std::sort(p.begin(), p.end(), [&](int i, int j) {
+    return comp(a[i], a[j]);
+  });
+  auto tmp = a;
+  for (int i = 0; i < int(a.size()); i++) {
+    a[i] = std::move(tmp[p[i]]);
+  }
+  return p;
+}
+
+template <class... Containers>
+std::vector<int> zip_sort(std::tuple<Containers&...> t) {
+  return std::apply([&](auto&... containers) {
+    int n = int(std::get<0>(t).size());
+    assert(((int(containers.size()) == n) && ...));
+
+    std::vector<int> p(n);
+    std::iota(p.begin(), p.end(), 0);
+    std::sort(p.begin(), p.end(), [&](int i, int j) {
+      return std::tie(containers[i]...) < std::tie(containers[j]...);
+    });
+
+    ([&](auto& container) {
+      auto tmp = container;
+      for (int i = 0; i < n; i++) {
+        container[i] = std::move(tmp[p[i]]);
+      }
+    }(containers), ...);
+
+    return p;
+  }, t);
+}
+
+template <class... Containers, class Comp>
+std::vector<int> zip_sort(std::tuple<Containers&...> t, Comp comp) {
+  return std::apply([&](auto&... containers) {
+    int n = int(std::get<0>(t).size());
+    assert(((int(containers.size()) == n) && ...));
+
+    std::vector<int> p(n);
+    std::iota(p.begin(), p.end(), 0);
+    std::sort(p.begin(), p.end(), [&](int i, int j) {
+      return comp(std::tie(containers[i]...), std::tie(containers[j]...));
+    });
+
+    ([&](auto& container) {
+      auto tmp = container;
+      for (int i = 0; i < n; i++) {
+        container[i] = std::move(tmp[p[i]]);
+      }
+    }(containers), ...);
+
+    return p;
+  }, t);
 }
 
 }
