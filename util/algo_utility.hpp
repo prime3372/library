fb08671644dae7d4f195499f16dc0b9a635935a0
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-17 00:33:28 +0900

    a

--- util/algo_utility.hpp
+++ util/algo_utility.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <cassert>
 #include <cstddef>
 #include <numeric>
 #include <utility>
@@ -16,23 +17,9 @@ template <class T> bool chmax(T& a, const T& b) {
   return b > a ? (a = b, true) : false;
 }
 
-template <class T> void uniq(T& a) {
+template <class Container> void uniq(Container& a) {
   std::sort(a.begin(), a.end());
   a.erase(std::unique(a.begin(), a.end()), a.end());
-}
-
-template <class Container>
-std::vector<int> sort(Container& a) {
-  std::vector<int> p(int(a.size()));
-  std::iota(p.begin(), p.end(), 0);
-  std::sort(p.begin(), p.end(), [&](int i, int j) {
-    return a[i] < a[j];
-  });
-  auto tmp = a;
-  for (int i = 0; i < int(a.size()); i++) {
-    a[i] = std::move(tmp[p[i]]);
-  }
-  return p;
 }
 
 template <class Container, class Comp>
@@ -49,27 +36,9 @@ std::vector<int> sort(Container& a, Comp comp) {
   return p;
 }
 
-template <class... Containers>
-std::vector<int> zip_sort(std::tuple<Containers&...> t) {
-  return std::apply([&](auto&... containers) {
-    int n = int(std::get<0>(t).size());
-    assert(((int(containers.size()) == n) && ...));
-
-    std::vector<int> p(n);
-    std::iota(p.begin(), p.end(), 0);
-    std::sort(p.begin(), p.end(), [&](int i, int j) {
-      return std::tie(containers[i]...) < std::tie(containers[j]...);
-    });
-
-    ([&](auto& container) {
-      auto tmp = container;
-      for (int i = 0; i < n; i++) {
-        container[i] = std::move(tmp[p[i]]);
-      }
-    }(containers), ...);
-
-    return p;
-  }, t);
+template <class Container>
+std::vector<int> sort(Container& a) {
+  return sort(a, std::less<std::decay_t<decltype(a[0])>>());
 }
 
 template <class... Containers, class Comp>
@@ -92,6 +61,13 @@ std::vector<int> zip_sort(std::tuple<Containers&...> t, Comp comp) {
     }(containers), ...);
 
     return p;
+  }, t);
+}
+
+template <class... Containers>
+std::vector<int> zip_sort(std::tuple<Containers&...> t) {
+  return std::apply([&](auto&... containers) {
+    return zip_sort(t, std::less<decltype(std::make_tuple(containers[0]...))>());
   }, t);
 }
 
