2b549321b8bc4ad18995601d155357366bf67eb8
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-17 04:23:59 +0900

    a

--- string/rolling_hash.hpp
+++ string/rolling_hash.hpp
@@ -13,12 +13,13 @@ namespace cp {
 struct rolling_hash {
 public:
   rolling_hash() : n(-1) {}
-  template <class Str> explicit rolling_hash(const Str& s) : n(int(s.size())) {
+  template <class Str>
+  explicit rolling_hash(const Str& s) : n(int(s.size())) {
     hs.resize(n + 1);
     pw.resize(n + 1);
     pw[0] = 1;
     for (int i = 0; i < n; i++) {
-      hs[i + 1] = hs[i] * basis + safe_hash<std::decay_t<decltype(s[i])>>()(s[i]);
+      hs[i + 1] = hs[i] * basis + hash_char(s[i]);
       pw[i + 1] = pw[i] * basis;
     }
   }
@@ -31,7 +32,7 @@ public:
   template <class Str> static hash61 to_hash(const Str& s) {
     hash61 h = 0;
     for (int i = 0; i < int(s.size()); i++) {
-      h = h * basis + safe_hash<std::decay_t<decltype(s[i])>>()(s[i]);
+      h = h * basis + hash_char(s[i]);
     }
     return h;
   }
@@ -40,6 +41,11 @@ private:
   static hash61 basis;
   int n;
   std::vector<hash61> hs, pw;
+
+  template <class T>
+  unsigned long long hash_char(const T& c) {
+    return safe_hash<T>()(c);
+  }
 };
 
 hash61 rolling_hash::basis = hash61::get_basis();
