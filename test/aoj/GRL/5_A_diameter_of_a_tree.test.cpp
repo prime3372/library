02de426c5730df20e68fc8f7bffa12acb048d15a
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-18 14:47:06 +0900

    add a test

---
+++ test/aoj/GRL/5_A_diameter_of_a_tree.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/GRL/all/GRL_5_A"
+
+#include "tree/tree_diameter.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cp;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  tree_diameter<int> diam(n);
+  for (int i = 0; i < n - 1; i++) {
+    int s, t, w;
+    cin >> s >> t >> w;
+    diam.add_edge(s, t, w);
+  }
+  diam.build();
+  cout << diam.len << "\n";
+}
