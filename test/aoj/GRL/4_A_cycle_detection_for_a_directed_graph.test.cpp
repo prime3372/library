0184c23c392a21804eb9417c85f7e3cf6870e1e8
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-18 14:33:20 +0900

    add a test

---
+++ test/aoj/GRL/4_A_cycle_detection_for_a_directed_graph.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/GRL/all/GRL_3_C"
+
+#include "graph/cycle_detection.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cp;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  cycle_detection<true> cycle(n);
+  for (int i = 0; i < m; i++) {
+    int s, t;
+    cin >> s >> t;
+    cycle.add_edge(s, t);
+  }
+  cycle.detect();
+  cout << (cycle.len != 0) << "\n";
+}
