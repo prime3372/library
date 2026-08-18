5e20f8497c6a4bc7dc07a65d9097859bc67df544
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-18 16:55:27 +0900

    add a test

---
+++ test/aoj/GRL/7_A_bipartite_matching.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/GRL/all/GRL_6_A"
+
+#include "graph/bipartite_matching.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cp;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int x, y, m;
+  cin >> x >> y >> m;
+  bipartite_matching bm(x, y);
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    bm.add_edge(u, v);
+  }  
+  cout << bm.init() << "\n";
+}
