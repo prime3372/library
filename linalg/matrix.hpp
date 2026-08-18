5c25a54eb17a8ef73261eb1ad5a8d6fb873ab83c
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-18 13:45:24 +0900

    add operator>> overload for input

--- linalg/matrix.hpp
+++ linalg/matrix.hpp
@@ -84,6 +84,12 @@ template <class T> class matrix {
     return r;
   }
 
+  friend std::istream& operator>>(std::istream& is, matrix& mat) {
+    for (int i = 0; i < mat.h; i++) {
+      for (int j = 0; j < mat.w; j++) is >> mat[i][j];
+    }
+    return is;
+  }
   friend std::ostream& operator<<(std::ostream& os, const matrix& mat) {
     for (int i = 0; i < mat.h; i++) {
       for (int j = 0; j < mat.w; j++) {
