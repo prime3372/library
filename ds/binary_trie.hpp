2eb8c62ab884732125d12a797177db94afc7880d
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-18 14:05:18 +0900

    fix

--- ds/binary_trie.hpp
+++ ds/binary_trie.hpp
@@ -12,9 +12,9 @@ namespace cp {
 template <class T> requires internal::is_unsigned_int_v<T>
 class binary_trie : public trie_tree<2, '0'> {
   using base = trie_tree<2, '0'>;
-  using base::trie_tree;
 
  public:
+  using base::trie_tree;
   int insert(T x) { return base::insert(std::bitset<bit>(x).to_string()); }
   bool erase(T x) { return base::erase(std::bitset<bit>(x).to_string()); }
   int count(T x) { return base::count(std::bitset<bit>(x).to_string()); }
