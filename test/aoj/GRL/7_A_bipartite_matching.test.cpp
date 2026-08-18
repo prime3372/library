5035640fc35ff28ae2e1f7db9d0cd345c1f8ccd9
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-18 17:03:00 +0900

    fix

--- test/aoj/GRL/7_A_bipartite_matching.test.cpp
+++ test/aoj/GRL/7_A_bipartite_matching.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/GRL/all/GRL_6_A"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/GRL/all/GRL_7_A"
 
 #include "graph/bipartite_matching.hpp"
 #include <iostream>
