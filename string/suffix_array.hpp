5e3438ef7ebe594938187e8c7a3ba57db36fa274
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-19 09:35:13 +0900

    fix

--- string/suffix_array.hpp
+++ string/suffix_array.hpp
@@ -46,8 +46,8 @@ template <class Str> std::vector<int> suffix_array(const Str& s) {
     rank_num = 1;
     nrank[nindex[0]] = 0;
     for (int i = 1; i < n; i++) {
-      long long cur = rank[nindex[i]] + rank[(nindex[i] + w) % n] * n;
-      long long pre = rank[nindex[i - 1]] + rank[(nindex[i - 1] + w) % n] * n;
+      long long cur = 1LL * rank[nindex[i]] * n + rank[(nindex[i] + w) % n];
+      long long pre = 1LL * rank[nindex[i - 1]] * n + rank[(nindex[i - 1] + w) % n];
       if (cur != pre) rank_num++;
       nrank[nindex[i]] = rank_num - 1;
     }
