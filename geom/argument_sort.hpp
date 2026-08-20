0f4724e32632cde3d249761449fb2a2ccaea2f3b
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-21 07:34:48 +0900

    a

--- geom/argument_sort.hpp
+++ geom/argument_sort.hpp
@@ -14,7 +14,6 @@ void argument_sort(std::vector<ipoint>& ps) {
     bool up_p = p.y > 0 || (p.y == 0 && p.x < 0);
     bool up_q = q.y > 0 || (q.y == 0 && q.x < 0);
     if (up_p != up_q) return !up_p && up_q;
-    if (p.x == 0 && p.y == 0) return false;
     if (q.x == 0 && q.y == 0) return p.y < 0;
     return cross(p, q) > 0;
   });
