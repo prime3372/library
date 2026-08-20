3940a83f0f8a885a9c786cc77df1098c0ecc4509
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-21 07:06:16 +0900

    a

--- ds/slide_window_aggregation.hpp
+++ ds/slide_window_aggregation.hpp
@@ -15,39 +15,39 @@ template <class M> class slide_window_aggregation {
   void push(const S& x) { push1(x); }
   void pop() {
     assert(!empty());
-    if (a0.empty()) transfer();
-    a0.pop_back();
+    if (val0.empty()) transfer();
+    val0.pop_back();
     cum0.pop_back();
     prod0 = cum0.empty() ? M::e() : cum0.back();
   }
 
   S front() const {
     assert(!empty());
-    return a0.empty() ? a1.front() : a0.back();
+    return val0.empty() ? val1.front() : val0.back();
   }
   S prod() const { return M::op(prod0, prod1); }
 
-  int size() const { return int(a0.size() + a1.size()); }
+  int size() const { return int(val0.size() + val1.size()); }
   bool empty() const { return size() == 0; }
 
  private:
-  std::vector<S> a0, a1, cum0, cum1;
+  std::vector<S> val0, val1, cum0, cum1;
   S prod0, prod1;
 
   void push0(S x) {
-    a0.push_back(x);
+    val0.push_back(x);
     cum0.push_back(prod0 = M::op(x, prod0));
   }
 
   void push1(S x) {
-    a1.push_back(x);
+    val1.push_back(x);
     cum1.push_back(prod1 = M::op(prod1, x));
   }
 
   void transfer() {
-    while (!a1.empty()) {
-      push0(a1.back());
-      a1.pop_back();
+    while (!val1.empty()) {
+      push0(val1.back());
+      val1.pop_back();
       cum1.pop_back();
     }
     prod1 = M::e();
--- ds/slide_window_aggregation_deque.hpp
+++ ds/slide_window_aggregation_deque.hpp
@@ -19,60 +19,60 @@ template <class M> class slide_window_aggregation_deque {
 
   void pop_front() {
     assert(!empty());
-    if (a0.empty()) rebalance();
-    a0.pop_back();
+    if (val0.empty()) rebalance();
+    val0.pop_back();
     cum0.pop_back();
     prod0 = cum0.empty() ? M::e() : cum0.back();
   }
   void pop_back() {
     assert(!empty());
-    if (a1.empty()) rebalance();
-    a1.pop_back();
+    if (val1.empty()) rebalance();
+    val1.pop_back();
     cum1.pop_back();
     prod1 = cum1.empty() ? M::e() : cum1.back();
   }
 
   S front() const {
     assert(!empty());
-    return a0.empty() ? a1.front() : a0.back();
+    return val0.empty() ? val1.front() : val0.back();
   }
   S back() const {
     assert(!empty());
-    return a1.empty() ? a0.front() : a1.back();
+    return val1.empty() ? val0.front() : val1.back();
   }
   S prod() const { return M::op(prod0, prod1); }
 
-  int size() { return int(a0.size() + a1.size()); }
+  int size() { return int(val0.size() + val1.size()); }
   bool empty() { return size() == 0; }
 
  private:
-  std::vector<S> a0, a1, cum0, cum1;
+  std::vector<S> val0, val1, cum0, cum1;
   S prod0, prod1;
 
   void push0(S x) {
-    a0.push_back(x);
+    val0.push_back(x);
     cum0.push_back(prod0 = M::op(x, prod0));
   }
 
   void push1(S x) {
-    a1.push_back(x);
+    val1.push_back(x);
     cum1.push_back(prod1 = M::op(prod1, x));
   }
 
   void rebalance() {
-    int n = int(a0.size() + a1.size());
-    int s0 = n / 2 + (a0.empty() ? n % 2 : 0);
-    std::vector<S> a = a0;
-    std::reverse(a.begin(), a.end());
-    std::copy(a1.begin(), a1.end(), std::back_inserter(a));
-    a0.clear();
-    a1.clear();
+    int n = int(val0.size() + val1.size());
+    int s0 = n / 2 + (val0.empty() ? n % 2 : 0);
+    std::vector<S> tmp = val0;
+    std::reverse(tmp.begin(), val0.end());
+    std::copy(val1.begin(), val1.end(), std::back_inserter(tmp));
+    val0.clear();
+    val1.clear();
     cum0.clear();
     cum1.clear();
     prod0 = M::e();
     prod1 = M::e();
-    for (int i = s0 - 1; i >= 0; i--) push0(a[i]);
-    for (int i = s0; i < n; i++) push1(a[i]);
+    for (int i = s0 - 1; i >= 0; i--) push0(tmp[i]);
+    for (int i = s0; i < n; i++) push1(tmp[i]);
   }
 };
 
