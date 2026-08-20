f00814f0988870942310200f8d0413e22c325d26
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-20 09:14:08 +0900

    simplify algo_utility

--- util/algo_utility.hpp
+++ util/algo_utility.hpp
@@ -17,62 +17,25 @@ template <class T> bool chmax(T& a, const T& b) {
   return b > a ? (a = b, true) : false;
 }
 
-template <class Container> void uniq(Container& a) {
-  std::sort(a.begin(), a.end());
-  a.erase(std::unique(a.begin(), a.end()), a.end());
-}
-
 template <class Container, class Comp>
 std::vector<int> sort(Container& a, Comp comp) {
   std::vector<int> p(int(a.size()));
   std::iota(p.begin(), p.end(), 0);
   std::sort(p.begin(), p.end(), [&](int i, int j) { return comp(a[i], a[j]); });
-  auto tmp = a;
+  auto a2 = a;
   for (int i = 0; i < int(a.size()); i++) {
-    a[i] = std::move(tmp[p[i]]);
+    a[i] = std::move(a2[p[i]]);
   }
   return p;
 }
 
 template <class Container> std::vector<int> sort(Container& a) {
-  return sort(a, std::less<std::decay_t<decltype(a[0])>>());
+  return sort(a, std::less<typename Container::value_type>());
 }
 
-template <class... Containers, class Comp>
-std::vector<int> zip_sort(std::tuple<Containers&...> t, Comp comp) {
-  return std::apply(
-      [&](auto&... containers) {
-        int n = int(std::get<0>(t).size());
-        assert(((int(containers.size()) == n) && ...));
-
-        std::vector<int> p(n);
-        std::iota(p.begin(), p.end(), 0);
-        std::sort(p.begin(), p.end(), [&](int i, int j) {
-          return comp(std::tie(containers[i]...), std::tie(containers[j]...));
-        });
-
-        (
-            [&](auto& container) {
-              auto tmp = container;
-              for (int i = 0; i < n; i++) {
-                container[i] = std::move(tmp[p[i]]);
-              }
-            }(containers),
-            ...);
-
-        return p;
-      },
-      t);
-}
-
-template <class... Containers>
-std::vector<int> zip_sort(std::tuple<Containers&...> t) {
-  return std::apply(
-      [&](auto&... containers) {
-        auto comp = std::less<decltype(std::make_tuple(containers[0]...))>();
-        return zip_sort(t, comp);
-      },
-      t);
+template <class Container> void unique(Container& a) {
+  std::sort(a.begin(), a.end());
+  a.erase(std::unique(a.begin(), a.end()), a.end());
 }
 
 }  // namespace cp
