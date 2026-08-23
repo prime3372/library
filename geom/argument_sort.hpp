a519e044c8ea4f2a5319e2555b5823abda4871ef
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-23 13:39:20 +0900

    a

--- geom/argument_sort.hpp
+++ geom/argument_sort.hpp
@@ -10,8 +10,7 @@ namespace cp {
 
 // sort by atan2
 // @note atan2(x < 0, y = 0) = pi and atan2(0, 0) = 0
-template <class T> requires internal::is_integral_v<T>
-void argument_sort(std::vector<point<T>>& ps) {
+template <class T> void argument_sort(std::vector<point<T>>& ps) {
   std::sort(ps.begin(), ps.end(), [](const point<T>& p, const point<T>& q) {
     bool up_p = p.y > 0 || (p.y == 0 && p.x < 0);
     bool up_q = q.y > 0 || (q.y == 0 && q.x < 0);
