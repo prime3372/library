ebe1819420c39c455a703c7662c0773ae192f03a
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-24 21:10:06 +0900

    a

--- test/yosupo/string/wildcard_pattern_matching.test.cpp
+++ test/yosupo/string/wildcard_pattern_matching.test.cpp
@@ -12,38 +12,26 @@ using ll = long long;
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
-  int n, m;
-  vector<ll> s, t;
-  {
-    string _s, _t;
-    cin >> _s >> _t;
-    n = int(_s.size());
-    m = int(_t.size());
-    s.resize(n); t.resize(m);
-    for (int i = 0; i < n; i++) {
-      if (_s[i] == '*') s[i] = 0;
-      else s[i] = _s[i] - 'a' + 1;
-    }
-    for (int i = 0; i < m; i++) {
-      if (_t[i] == '*') t[i] = 0;
-      else t[i] = _t[i] - 'a' + 1;
-    }
+  string s, t;
+  cin >> s >> t;
+  int n = int(s.size()), m = int(t.size());
+  vector<ll> a1(n), a2(n), a3(n), b1(m), b2(m), b3(m);
+  for (int i = 0; i < n; i++) {
+    a1[i] = s[i] == '*' ? 0 : s[i] - 'a' + 1;
   }
-
-  vector<ll> s2(n), s3(n), t2(m), t3(m);
-  for (int i = 0; i < n; i++) s2[i] = s[i] * s[i];
-  for (int i = 0; i < n; i++) s3[i] = s[i] * s[i] * s[i];
-  for (int i = 0; i < m; i++) t2[i] = t[i] * t[i];
-  for (int i = 0; i < m; i++) t3[i] = t[i] * t[i] * t[i];
-
-  reverse(t.begin(), t.end());
-  reverse(t2.begin(), t2.end());
-  reverse(t3.begin(), t3.end());
-
-  auto c1 = convolution_ll(s3, t);
-  auto c2 = convolution_ll(s2, t2);
-  auto c3 = convolution_ll(s, t3);
-
+  for (int i = 0; i < m; i++) {
+    b1[i] = t[i] == '*' ? 0 : t[i] - 'a' + 1;
+  }
+  for (int i = 0; i < n; i++) a2[i] = a1[i] * a1[i];
+  for (int i = 0; i < n; i++) a3[i] = a2[i] * a1[i];
+  for (int i = 0; i < m; i++) b2[i] = b1[i] * b1[i];
+  for (int i = 0; i < m; i++) b3[i] = b2[i] * b1[i];
+  reverse(b1.begin(), b1.end());
+  reverse(b2.begin(), b2.end());
+  reverse(b3.begin(), b3.end());
+  auto c1 = convolution_ll(a3, b1);
+  auto c2 = convolution_ll(a2, b2);
+  auto c3 = convolution_ll(a1, b3);
   vector<ll> ans(n - m + 1);
   for (int i = 0; i < n - m + 1; i++) {
     ans[i] = c1[m - 1 + i] - 2 * c2[m - 1 + i] + c3[m - 1 + i];
