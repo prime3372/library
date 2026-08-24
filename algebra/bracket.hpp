307c9d78dad43ac89dc2d2c0f4921628a7903617
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-24 23:43:10 +0900

    a

--- algebra/bracket.hpp
+++ algebra/bracket.hpp
@@ -9,14 +9,14 @@ namespace alg {
 
 struct bracket {
   struct S {
-    int prefix_min, sum;
-    bool is_correct() const { return prefix_min >= 0 && sum == 0; }
+    int sum, prefix_min, suffix_max;
   };
 
   static S op(S x, S y) {
-    return S{std::min(x.prefix_min, x.sum + y.prefix_min), x.sum + y.sum};
+    return S{x.sum + y.sum, std::min(x.prefix_min, x.sum + y.prefix_min),
+             std::max(x.suffix_max + y.sum, y.suffix_max)};
   }
-  static S e() { return S{0, 0}; }
+  static S e() { return S{0, 0, 0}; }
 };
 
 }  // namespace alg
