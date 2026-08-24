879adb4ae5c66b1d5351f0e07804df2d10d9f082
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-24 23:53:20 +0900

    a

--- algebra/bracket.hpp
+++ algebra/bracket.hpp
@@ -10,13 +10,19 @@ namespace alg {
 struct bracket {
   struct S {
     int sum, prefix_min, suffix_max;
+    S(int x) : sum(x), prefix_min(x), suffix_max(x) {}
+    S(int s, int pre, int suf) : sum(s), prefix_min(pre), suffix_max(suf) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const S& x) {
+      return os << x.sum;
+    }
   };
 
   static S op(S x, S y) {
     return S{x.sum + y.sum, std::min(x.prefix_min, x.sum + y.prefix_min),
              std::max(x.suffix_max + y.sum, y.suffix_max)};
   }
-  static S e() { return S{0, 0, 0}; }
+  static S e() { return S{0}; }
 };
 
 }  // namespace alg
