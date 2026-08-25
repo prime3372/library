6a225989e2b2251ee86d2f2fa55b99359d615e05
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-25 10:06:47 +0900

    a

--- template/template.cpp
+++ template/template.cpp
@@ -1,9 +1,9 @@
 #include "template/template.hpp"
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
   iflocal {
     cout << unitbuf;
   }
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
 }
