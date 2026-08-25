fed76fe8bf34cbadeaa7efd5c845d8bd0213b28a
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-25 15:48:58 +0900

    add comment

--- setfunc/subset_zeta_mobius.hpp
+++ setfunc/subset_zeta_mobius.hpp
@@ -5,8 +5,18 @@
 
 namespace cp {
 
+// subset zeta transfrom
 template <class T> void subset_zeta(int n, std::vector<T>& a) {
   assert(0 <= n && int(a.size()) == 1 << n);
+  // example: n = 8
+  // [000] -> [000] -> [000] -> [000]
+  // [001] -> [00*] -> [00*] -> [00*]
+  // [010] -> [010] -> [0*0] -> [0*0]
+  // [011] -> [01*] -> [0**] -> [0**]
+  // [100] -> [100] -> [100] -> [*00]
+  // [101] -> [10*] -> [10*] -> [*0*]
+  // [110] -> [110] -> [1*0] -> [**0]
+  // [111] -> [11*] -> [1**] -> [***]
   for (int i = 0; i < n; i++) {
     int b = 1 << i;
     for (int l = 0; l < (1 << n); l += 2 * b) {
@@ -17,8 +27,18 @@ template <class T> void subset_zeta(int n, std::vector<T>& a) {
   }
 }
 
+// subset mobius transform
 template <class T> void subset_mobius(int n, std::vector<T>& a) {
   assert(0 <= n && int(a.size()) == 1 << n);
+  // example: n = 8
+  // [000] -> [000] -> [000] -> [000]
+  // [00*] -> [001] -> [001] -> [001]
+  // [0*0] -> [0*0] -> [010] -> [010]
+  // [0**] -> [0*1] -> [011] -> [011]
+  // [*00] -> [*00] -> [*00] -> [100]
+  // [*0*] -> [*01] -> [*01] -> [101]
+  // [**0] -> [**0] -> [*10] -> [110]
+  // [***] -> [**1] -> [*11] -> [111]
   for (int i = 0; i < n; i++) {
     int b = 1 << i;
     for (int l = 0; l < (1 << n); l += 2 * b) {
--- setfunc/superset_zeta_mobius.hpp
+++ setfunc/superset_zeta_mobius.hpp
@@ -5,8 +5,18 @@
 
 namespace cp {
 
+// superset zeta transform
 template <class T> void superset_zeta(int n, std::vector<T>& a) {
   assert(0 <= n && int(a.size()) == 1 << n);
+  // example: n = 8
+  // [000] -> [00*] -> [0**] -> [***]
+  // [001] -> [001] -> [0*1] -> [**1]
+  // [010] -> [01*] -> [01*] -> [*1*]
+  // [011] -> [011] -> [011] -> [*11]
+  // [100] -> [10*] -> [1**] -> [1**]
+  // [101] -> [101] -> [1*1] -> [1*1]
+  // [110] -> [11*] -> [11*] -> [11*]
+  // [111] -> [111] -> [111] -> [111]
   for (int i = 0; i < n; i++) {
     int b = 1 << i;
     for (int l = 0; l < (1 << n); l += 2 * b) {
@@ -17,8 +27,18 @@ template <class T> void superset_zeta(int n, std::vector<T>& a) {
   }
 }
 
+// superset mobius transform
 template <class T> void superset_mobius(int n, std::vector<T>& a) {
   assert(0 <= n && int(a.size()) == 1 << n);
+  // example: n = 8
+  // [***] -> [**0] -> [*00] -> [000]
+  // [**1] -> [**1] -> [*01] -> [001]
+  // [*1*] -> [*10] -> [*10] -> [010]
+  // [*11] -> [*11] -> [*11] -> [011]
+  // [1**] -> [1*0] -> [100] -> [100]
+  // [1*1] -> [1*1] -> [101] -> [101]
+  // [11*] -> [110] -> [110] -> [110]
+  // [111] -> [111] -> [111] -> [111]
   for (int i = 0; i < n; i++) {
     int b = 1 << i;
     for (int l = 0; l < (1 << n); l += 2 * b) {
