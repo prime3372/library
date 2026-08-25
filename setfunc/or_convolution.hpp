42bce4b2886c5fa1b0078a592e9be15fcd1096b7
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-25 13:59:22 +0900

    fix typo

--- setfunc/or_convolution.hpp
+++ setfunc/or_convolution.hpp
@@ -10,7 +10,7 @@ template <class T>
 std::vector<T> or_convolution(int n, std::vector<T> a, std::vector<T> b) {
   subset_zeta(n, a);
   subset_zeta(n, b);
-  for (int i = 0; i < (1 << i); i++) a[i] *= b[u];
+  for (int i = 0; i < (1 << n); i++) a[i] *= b[u];
   subset_mobius(n, a);
   return a;
 }
