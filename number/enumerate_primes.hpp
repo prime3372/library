1af85b15da64b2221ef389bd2206557fdbca22a3
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-25 13:57:04 +0900

    a

--- number/enumerate_primes.hpp
+++ number/enumerate_primes.hpp
@@ -8,16 +8,16 @@ namespace cp {
 // @param n `0 <= n`
 std::vector<int> enumerate_primes(int n) {
   assert(0 <= n);
-  std::vector<int> ps;
-  std::vector<bool> prime(n + 1, true);
+  std::vector<int> ans;
+  std::vector<bool> sieve(n + 1, true);
   for (int i = 2; i <= n; i++) {
-    if (!prime[i]) continue;
-    ps.push_back(i);
+    if (!sieve[i]) continue;
+    ans.push_back(i);
     for (long long j = 1LL * i * i; j <= n; j += i) {
-      prime[j] = false;
+      sieve[j] = false;
     }
   }
-  return ps;
+  return ans;
 }
 
 }  // namespace cp
