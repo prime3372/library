6fab97e3e77179405a2bda3775571b1543b9657d
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-28 03:20:10 +0900

    refactor

--- tree/tree_distance.hpp
+++ tree/tree_distance.hpp
@@ -4,35 +4,42 @@
 #include <utility>
 #include <vector>
 
-#include "tree/lowest_common_ancestor.hpp"
+#include "tree/hl_decomposition.hpp"
 
 namespace cp {
 
 template <class T> class tree_distance {
  public:
   tree_distance() : n(0) {}
-  explicit tree_distance(int _n) : n(_n), cost(_n), lca(_n) {}
+  explicit tree_distance(int _n) : n(_n), g(_n), cost(_n), hld(_n) {}
 
   void add_edge(int a, int b, T c) {
     assert(0 <= a && a < n);
     assert(0 <= b && b < n);
     g[a].emplace_back(b, c);
     g[b].emplace_back(a, c);
-    lca.add_edge(a, b);
+    hld.add_edge(a, b);
   }
 
   void init() {
-    lca.init(0);
+    assert(n != 0);
+    hld.build(0);
     dfs(0, -1, 0);
     initialized = true;
   }
 
-  T operator()(int a, int b) {
-    assert(initialized);
+  int operator()(int a, int b) {
     assert(0 <= a && a < n);
     assert(0 <= b && b < n);
-    int l = lca(a, b);
-    return cost[a] + cost[b] - cost[l] * 2;
+    if (!initialized) init();
+    a = hld.ord[a];
+    b = hld.ord[b];
+    while (hld.head[a] != hld.head[b]) {
+      if (a > b) std::swap(a, b);
+      b = hld.next[b];
+    }
+    int l = hld.vertex[std::min(a, b)];
+    return cost[a] + cost[b] - cost[c] * 2;
   }
 
  private:
@@ -40,7 +47,7 @@ template <class T> class tree_distance {
   bool initialized = false;
   std::vector<std::vector<std::pair<int, T>>> g;
   std::vector<T> cost;
-  lowest_common_ancestor lca;
+  hl_decomposition hld;
 
   void dfs(int v, int pv, T c) {
     cost[v] = c;
