aa587e844022239557e8c8a9adc660196dd5096b
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-27 19:59:05 +0900

    update debug macro

--- template/template.hpp
+++ template/template.hpp
@@ -29,22 +29,32 @@ template <class T, class U> using dict = hash_map<T, U>;
 
 #ifdef LOCAL
 
-#define GET_STRING(a) #a
+#define GET_STRING(a, ...) #a
 
 #define local 1
-#define debug(...)                                                         \
-  do {                                                                     \
-    std::cerr << GET_STRING(__VA_ARGS__) << ": " << (__VA_ARGS__) << "\n"; \
-  } while (0)
-#define write(...)                            \
-  []<class... _Args>(const _Args&... _args) { \
-    (std::cerr << ... << _args);              \
+#define dump(...)                                            \
+  []<class... _Args>(const _Args&... _args) -> void {        \
+    string _args_str = GET_STRING((__VA_ARGS__));            \
+    if (sizeof...(_Args) <= 1) {                             \
+      _args_str = _args_str.substr(1, _args_str.size() - 2); \
+    }                                                        \
+    _args_str += ": ";                                       \
+    cerr << _args_str;                                       \
+    ostringstream _oss;                                      \
+    _oss << make_tuple(_args...);                            \
+    string _outs = _oss.str();                               \
+    if (count(_outs.begin(), _outs.end(), '\n')) {           \
+      cerr << "\n";                                          \
+    }                                                        \
+    cerr << _outs << "\n";                                   \
   }(__VA_ARGS__)
+#define write(...) \
+  [](const auto&... _args) -> void { (cerr << ... << _args); }(__VA_ARGS__)
 
 #else
 
 #define local 0
-#define debug(...)
+#define dump(...)
 #define write(...)
 
 #endif
