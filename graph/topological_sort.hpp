c0003d37bf332271408b3f3283025909b679ab69
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-28 22:10:30 +0900

    fix comment

--- graph/topological_sort.hpp
+++ graph/topological_sort.hpp
@@ -7,7 +7,7 @@
 
 namespace cp {
 
-// @note if g has a cycle, return an empty array
+// @note if g has cycles, return an empty array
 std::vector<int> topological_sort(const std::vector<std::vector<int>>& g) {
   int n = int(g.size());
   std::vector<int> in_deg(n);
