c554e3d7099f1844dbf1af5eeb8b8c6e13f09fad
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-31 01:39:18 +0900

    refactor ds

--- ds/lazy_segtree.hpp
+++ ds/lazy_segtree.hpp
@@ -12,20 +12,18 @@
 
 namespace cp {
 
-template <class M> class lazy_segtree {
-  using S = typename M::S;
-  using F = typename M::F;
-
+template <class S, auto op, auto e, class F, auto act, auto compose, auto id>
+class lazy_segtree {
  public:
   lazy_segtree() : lazy_segtree(0) {}
-  explicit lazy_segtree(int _n) : lazy_segtree(_n, M::e()) {}
+  explicit lazy_segtree(int _n) : lazy_segtree(_n, e()) {}
   explicit lazy_segtree(int _n, const S& val)
       : lazy_segtree(std::vector<S>(_n, val)) {}
   explicit lazy_segtree(const std::vector<S>& v) : n(int(v.size())) {
     sz = int(std::bit_ceil((unsigned int)(n)));
     log = std::countr_zero((unsigned int)(sz));
     d = std::vector<S>(2 * sz);
-    lz = std::vector<F>(sz, M::id());
+    lz = std::vector<F>(sz, id());
     for (int i = 0; i < n; i++) d[sz + i] = v[i];
     for (int i = sz - 1; i >= 1; i--) update(i);
   }
@@ -47,7 +45,7 @@ template <class M> class lazy_segtree {
 
   S prod(int l, int r) {
     assert(0 <= l && l <= r && r <= n);
-    if (l == r) return M::e();
+    if (l == r) return e();
 
     l += sz;
     r += sz;
@@ -57,15 +55,15 @@ template <class M> class lazy_segtree {
       if (((r >> i) << i) != r) push((r - 1) >> i);
     }
 
-    S prodl = M::e(), prodr = M::e();
+    S prodl = e(), prodr = e();
     while (l < r) {
-      if (l & 1) prodl = M::op(prodl, d[l++]);
-      if (r & 1) prodr = M::op(d[--r], prodr);
+      if (l & 1) prodl = op(prodl, d[l++]);
+      if (r & 1) prodr = op(d[--r], prodr);
       l >>= 1;
       r >>= 1;
     }
 
-    return M::op(prodl, prodr);
+    return op(prodl, prodr);
   }
 
   S all_prod() const { return d[1]; }
@@ -74,7 +72,7 @@ template <class M> class lazy_segtree {
     assert(0 <= i && i < n);
     i += sz;
     for (int j = log; j >= 1; j--) push(i >> j);
-    d[i] = M::mapping(f, d[i]);
+    d[i] = act(f, d[i]);
     for (int j = 1; j <= log; j++) update(i >> j);
   }
 
@@ -110,25 +108,25 @@ template <class M> class lazy_segtree {
 
   template <class G> int max_right(int l, G g) {
     assert(0 <= l && l <= n);
-    assert(g(M::e()));
+    assert(g(e()));
     if (l == n) return n;
     l += sz;
     for (int i = log; i >= 1; i--) push(l >> i);
-    S product = M::e();
+    S product = e();
     do {
       while (l % 2 == 0) l >>= 1;
-      if (!g(M::op(product, d[l]))) {
+      if (!g(op(product, d[l]))) {
         while (l < sz) {
           push(l);
           l = 2 * l;
-          if (g(M::op(product, d[l]))) {
-            product = M::op(product, d[l]);
+          if (g(op(product, d[l]))) {
+            product = op(product, d[l]);
             l++;
           }
         }
         return l - sz;
       }
-      product = M::op(product, d[l]);
+      product = op(product, d[l]);
       l++;
     } while ((l & -l) != l);
     return n;
@@ -136,26 +134,26 @@ template <class M> class lazy_segtree {
 
   template <class G> int min_left(int r, G g) {
     assert(0 <= r && r <= n);
-    assert(g(M::e()));
+    assert(g(e()));
     if (r == 0) return 0;
     r += sz;
     for (int i = log; i >= 1; i--) push((r - 1) >> i);
-    S product = M::e();
+    S product = e();
     do {
       r--;
       while (r > 1 && r % 2) r >>= 1;
-      if (!g(M::op(d[r], product))) {
+      if (!g(op(d[r], product))) {
         while (r < sz) {
           push(r);
           r = 2 * r + 1;
-          if (g(M::op(d[r], product))) {
-            product = M::op(d[r], product);
+          if (g(op(d[r], product))) {
+            product = op(d[r], product);
             r--;
           }
         }
         return r + 1 - sz;
       }
-      product = M::op(d[r], product);
+      product = op(d[r], product);
     } while ((r & -r) != r);
     return 0;
   }
@@ -178,15 +176,15 @@ template <class M> class lazy_segtree {
   std::vector<S> d;
   std::vector<F> lz;
 
-  void update(int k) { d[k] = M::op(d[2 * k], d[2 * k + 1]); }
+  void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
   void all_apply(int k, F f) {
-    d[k] = M::mapping(f, d[k]);
-    if (k < sz) lz[k] = M::composition(f, lz[k]);
+    d[k] = act(f, d[k]);
+    if (k < sz) lz[k] = compose(f, lz[k]);
   }
   void push(int k) {
     all_apply(2 * k, lz[k]);
     all_apply(2 * k + 1, lz[k]);
-    lz[k] = M::id();
+    lz[k] = id();
   }
 };
 
