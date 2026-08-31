1872924a91ff853e3536eaaec56c40f92ed88435
prime3372 <syunrurusera@yahoo.ne.jp>
2026-08-31 23:06:00 +0900

    add a test

---
+++ test/aoj/DSL/4_A_union_of_rectangles.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/4/DSL_4_A"
+
+#include "ds/area_of_union_of_rectangles.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cp;
+using ll = long long;
+
+int main() {
+  int n;
+  cin >> n;
+  area_of_union_of_rectangles<ll> area;
+  for (int i = 0; i < n; i++) {
+    ll x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    area.add_rect(x1, y1, x2, y2);
+  }
+  cout << area.calc() << "\n";
+}
