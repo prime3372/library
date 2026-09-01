7af9e6faaabe86f80af8774bfaa5e7d549d2ac0f
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-02 03:32:17 +0900

    refactor geom/area.hpp

--- geom/area.hpp
+++ geom/area.hpp
@@ -7,13 +7,16 @@
 
 namespace cp {
 
-// @return signed area of polygon p
-template <class T> long double area(const std::vector<point<T>>& p) {
+template <class T> long double signed_area(const std::vector<point<T>>& p) {
   long double ans = 0;
   for (int i = 0; i < int(p.size()); ++i) {
     ans += cross(p[i], p[(i + 1) % p.size()]);
   }
   return ans * 0.5;
+}
+
+template <class T> long double area(const std::vector<point<T>>& p) {
+  return std::abs(signed_area(p));
 }
 
 }  // namespace cp
