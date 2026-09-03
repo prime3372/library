3948093a553554ec0cb60a147ad5010aa81b3529
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-04 05:22:22 +0900

    refactor math utilities

--- util/math_utility.hpp
+++ util/math_utility.hpp
@@ -3,29 +3,31 @@
 #include <algorithm>
 #include <cassert>
 #include <cmath>
+#include <type_traits>
 
 namespace cp {
 
-long long ipow(long long x, long long n) {
+template <class T = long long> T ipow(long long x, long long n) {
   assert(0 <= n);
-  long long r = 1;
+  T r = 1, t = x;
   while (n) {
-    if (n & 1) r *= x;
-    x *= x;
+    if (n & 1) r *= t;
+    t *= t;
     n >>= 1;
   }
   return r;
 }
 
-unsigned long long isqrt(unsigned long long x) {
-  unsigned long long y = (unsigned long long)(std::sqrt(double(x)));
+template <class T> T isqrt(T x) {
+  T y = T(std::sqrt(double(x)));
+  if (sizeof(T) > 8) y = (y + x / y) / 2;  // Newton's method
   while (y != 0 && y > x / y) y--;
   while ((y + 1) <= x / (y + 1)) y++;
   return y;
 }
 
-unsigned long long icbrt(unsigned long long x) {
-  unsigned long long y = (unsigned long long)(std::cbrt(double(x)));
+template <class T> T icbrt(T x) {
+  T y = T(std::cbrt(double(x)));
   while (y != 0 && y * y > x / y) y--;
   while ((y + 1) * (y + 1) <= x / (y + 1)) y++;
   return y;
@@ -33,7 +35,7 @@ unsigned long long icbrt(unsigned long long x) {
 
 template <class T> int sign(T x) { return x < 0 ? -1 : x == 0 ? 0 : 1; }
 
-long long floor(long long n, long long d) {
+template <class T, class U> std::common_type_t<T, U> floor(T n, U d) {
   assert(d != 0);
   if (d < 0) {
     n = -n;
@@ -43,8 +45,12 @@ long long floor(long long n, long long d) {
   return n / d;
 }
 
-long long ceil(long long n, long long d) { return floor(n - sign(d), d) + 1; }
+template <class T, class U> std::common_type_t<T, U> ceil(T n, U d) {
+  return floor(n - sign(d), d) + 1;
+}
 
-long long mod(long long n, long long m) { return n - floor(n, m) * m; }
+template <class T, class U> std::common_type_t<T, U> mod(T n, U m) {
+  return n - floor(n, m) * m;
+}
 
 }  // namespace cp
