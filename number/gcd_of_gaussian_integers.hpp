7b359dfb852fdfb6b5e9e30527074ebdbf2fb39d
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-04 03:58:44 +0900

    a

--- number/gcd_of_gaussian_integers.hpp
+++ number/gcd_of_gaussian_integers.hpp
@@ -5,8 +5,7 @@
 namespace cp {
 
 gaussian_integer gcd(const gaussian_integer& z1, const gaussian_integer& z2) {
-  if (z2.x == 0 && z2.y == 0) return z1;
-  return gcd(z2, z1 % z2);
+  return (z2.x == 0 && z2.y == 0) ? z1 : gcd(z2, z1 % z2);
 }
 
 }  // namespace cp
