c28a76770f0a546c4b8da26ba10f02ff9142fb4f
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-04 13:24:55 +0900

    simplify `dump` macro

--- template/template.hpp
+++ template/template.hpp
@@ -31,18 +31,12 @@ using mint = cp::modint998244353;
 #define GET_STRING(a, ...) #a
 
 #define local 1
-#define dump(...)                                                   \
-  []<class... _Args>(const _Args&... _args) -> void {               \
-    std::string _args_str = GET_STRING((__VA_ARGS__));              \
-    if (sizeof...(_Args) <= 1)                                      \
-      _args_str = _args_str.substr(1, _args_str.size() - 2);        \
-    _args_str += ": ";                                              \
-    cerr << _args_str;                                              \
-    std::ostringstream _oss;                                        \
-    _oss << std::make_tuple(_args...);                              \
-    std::string _outs = _oss.str();                                 \
-    if (std::count(_outs.begin(), _outs.end(), '\n')) cerr << "\n"; \
-    cerr << _outs << "\n";                                          \
+#define dump(...)                                                        \
+  []<class... _Args>(const _Args&... _args) -> void {                    \
+    std::string _args_str = GET_STRING((__VA_ARGS__));                   \
+    if (sizeof...(_Args) <= 1)                                           \
+      _args_str = _args_str.substr(1, _args_str.size() - 2);             \
+    std::cerr << _args_str << ": " << std::make_tuple(_args...) << "\n"; \
   }(__VA_ARGS__)
 #define write(...) \
   [](const auto&... _args) -> void { (cerr << ... << _args); }(__VA_ARGS__)
