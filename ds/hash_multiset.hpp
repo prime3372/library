9446378799103e1e39382721d9cd526be4ef11b7
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-10 19:10:30 +0900

    add comment

--- ds/hash_multiset.hpp
+++ ds/hash_multiset.hpp
@@ -8,6 +8,7 @@
 
 namespace cp {
 
+// @note `Val` cannot be `bool`
 template <class T> class hash_multiset {
  public:
   hash_multiset() {}
--- ds/hash_set.hpp
+++ ds/hash_set.hpp
@@ -10,6 +10,7 @@
 
 namespace cp {
 
+// @note `Val` cannot be `bool`
 template <class T> class hash_set {
  public:
   hash_set() {}
