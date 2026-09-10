68bf86467d631a3037653c56c820c42d59eb58ce
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-10 17:16:11 +0900

    fix

--- ds/binary_trie.hpp
+++ ds/binary_trie.hpp
@@ -9,7 +9,7 @@
 
 namespace cp {
 
-template <class T> requires internal::is_unsigned_int_v<T>
+  template <class T> requires internal::is_unsigned_int_v<T>
 class binary_trie : public trie<2, '0'> {
   using base = trie<2, '0'>;
 
@@ -18,7 +18,9 @@ class binary_trie : public trie<2, '0'> {
   int insert(T x) { return base::insert(std::bitset<bit>(x).to_string()); }
   bool erase(T x) { return base::erase(std::bitset<bit>(x).to_string()); }
   int count(T x) { return base::count(std::bitset<bit>(x).to_string()); }
-  int prefix(T x) { return base::prefix(std::bitset<bit>(x).to_string()); }
+  int count_prefix(T x) {
+    return base::count_prefix(std::bitset<bit>(x).to_string());
+  }
 
  private:
   static constexpr int bit = std::numeric_limits<T>::digits;
