83df6752c96ebc13bfe247c0d717a2f0e28fd3a6
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-12 05:18:36 +0900

    a

--- tree/centroid_decomposition.hpp
+++ tree/centroid_decomposition.hpp
@@ -9,7 +9,7 @@ class centroid_decomposition {
  public:
   centroid_decomposition() : n(0) {}
   explicit centroid_decomposition(int _n)
-      : parent(_n, -1), size(_n), n(_n), g(_n), removed(_n) {}
+      : parent(_n, -1), size(_n), n(_n), g(_n), removed(_n), tmp_size(_n) {}
 
   void add_edge(int u, int v) {
     assert(0 <= u && u < n);
@@ -18,48 +18,42 @@ class centroid_decomposition {
     g[v].push_back(u);
   }
 
-  int root = -1;
   std::vector<int> parent, size, dfs_order;
 
-  centroid_decomposition& build() {
-    dfs_order.reserve(n);
-    root = build(0);
-    return *this;
-  }
-
- private:
-  int n;
-  std::vector<std::vector<int>> g;
-  std::vector<bool> removed;
-
-  void calc_size(int v, int pv) {
-    size[v] = 1;
-    for (int nv : g[v]) {
-      if (nv == pv || removed[nv]) continue;
-      calc_size(nv, v);
-      size[v] += size[nv];
-    }
-  }
-
-  int find_centroid(int v, int pv, int mid) {
-    for (int nv : g[v]) {
-      if (nv == pv || removed[nv]) continue;
-      if (size[nv] > mid) return find_centroid(nv, v, mid);
-    }
-    return v;
-  }
-
-  int build(int v) {
+  int build(int v = 0) {
     calc_size(v, -1);
-    int c = find_centroid(v, -1, size[v] / 2);
+    int c = find_centroid(v, -1, tmp_size[v] / 2);
     dfs_order.push_back(c);
-    size[c] = size[v];
+    size[c] = tmp_size[v];
     removed[c] = true;
     for (int to : g[c]) {
       if (removed[to]) continue;
       parent[build(to)] = c;
     }
     return c;
+  }
+
+ private:
+  int n;
+  std::vector<std::vector<int>> g;
+  std::vector<bool> removed;
+  std::vector<int> tmp_size;
+
+  void calc_size(int v, int pv) {
+    tmp_size[v] = 1;
+    for (int nv : g[v]) {
+      if (nv == pv || removed[nv]) continue;
+      calc_size(nv, v);
+      tmp_size[v] += tmp_size[nv];
+    }
+  }
+
+  int find_centroid(int v, int pv, int mid) {
+    for (int nv : g[v]) {
+      if (nv == pv || removed[nv]) continue;
+      if (tmp_size[nv] > mid) return find_centroid(nv, v, mid);
+    }
+    return v;
   }
 };
 
