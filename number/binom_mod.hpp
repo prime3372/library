f97b41fe71e24ce239a65345b218c3404c70f1ba
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-11 23:16:48 +0900

    add `multichoose` and tighten constraints

--- number/binom_mod.hpp
+++ number/binom_mod.hpp
@@ -3,25 +3,28 @@
 #include <cassert>
 #include <vector>
 
+#include "number/is_prime.hpp"
 #include "util/type_traits.hpp"
 
 namespace cp {
 
+// @note The mod must be prime.
 template <class mint> requires(internal::is_modint_v<mint>)
 class binom_mod {
  public:
   binom_mod() : binom_mod(1) {}
   explicit binom_mod(int n) : N(n) {
-    assert(0 <= N);
-    if (N == 0) N = 1;
-    f.resize(N + 1);
-    fi.resize(N + 1);
-    std::vector<mint> inv(N + 1);
+    static int m = mint::mod();
+    assert(is_prime(m));
+    assert(0 <= N && N < m);
+    int K = std::max(N, 1);
+    f.resize(K + 1);
+    fi.resize(K + 1);
+    std::vector<mint> inv(K + 1);
     inv[1] = 1;
     f[0] = f[1] = 1;
     fi[0] = fi[1] = 1;
-    for (int i = 2; i <= N; i++) {
-      int m = mint::mod();
+    for (int i = 2; i <= K; i++) {
       inv[i] = -inv[m % i] * (m / i);
       f[i] = f[i - 1] * i;
       fi[i] = fi[i - 1] * inv[i];
@@ -29,16 +32,21 @@ class binom_mod {
   }
 
   mint operator()(int n, int r) const {
-    if (r < 0 || n < r) return 0;
     assert(0 <= n && n <= N);
-    assert(fi[n - r] != 0 && fi[r] != 0);
+    if (r < 0 || n < r) return 0;
     return f[n] * fi[n - r] * fi[r];
   }
 
+  mint multichoose(int n, int r) const {
+    assert(0 <= n && n + r - 1 <= N);
+    if (n == 0) return r == 0;
+    if (r < 0) return 0;
+    return (*this)(n + r - 1, r);
+  }
+
   mint perm(int n, int r) const {
-    if (r < 0 || n < r) return 0;
     assert(0 <= n && n <= N);
-    assert(fi[n - r] != 0);
+    if (r < 0 || n < r) return 0;
     return f[n] * fi[n - r];
   }
 
@@ -49,7 +57,6 @@ class binom_mod {
 
   mint ifact(int n) const {
     assert(0 <= n && n <= N);
-    assert(fi[n] != 0);
     return fi[n];
   }
 
