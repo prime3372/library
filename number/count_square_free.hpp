cfabb59d17016b5c1204ea769a0785660df74640
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-12 23:11:03 +0900

    a

--- number/count_square_free.hpp
+++ number/count_square_free.hpp
@@ -26,19 +26,12 @@ long long count_square_free(long long n) {
   std::vector<long long> Mx(Imax);
   for (int i = Imax - 1; i >= 1; i--) {
     long long xi = isqrt(n / i), r = isqrt(xi), Mxi = 1;
-
     for (int j = 1; j <= xi / (r + 1); j++) {
       Mxi -= (xi / j - xi / (j + 1)) * M[j];
     }
-
     for (int j = 2; j <= r; j++) {
-      if (xi / j <= D) {
-        Mxi -= M[xi / j];
-      } else {
-        Mxi -= Mx[j * j * i];
-      }
+      Mxi -= (xi / j <= D ? M[xi / j] : Mx[j * j * i]);
     }
-
     Mxi_sum += Mxi;
     Mx[i] = Mxi;
   }
