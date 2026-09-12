0ef29521c8f87747de9ef7442928297891421bea
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-12 23:06:22 +0900

    create count_divisors.hpp

--- number/count_divisors.hpp
+++ number/count_divisors.hpp
@@ -1,30 +1,18 @@
 #pragma once
 
-#include <algorithm>
 #include <cassert>
-#include <utility>
-#include <vector>
 
 #include "number/factorize.hpp"
 
 namespace cp {
 
 // @param n `1 <= n`
-// @note The order of the divisors is undefined.
-std::vector<long long> enumerate_divisors(long long n) {
+long long count_divisors(long long n) {
   assert(1 <= n);
-  std::vector<long long> ans = {1};
   auto factorized = factorize(n);
-  long long ans = 0;
+  long long ans = 1;
   for (auto& [p, e] : factorized) {
-    int sz = int(ans.size());
-    long long cur = 1;
-    for (int i = 0; i < e; i++) {
-      cur *= p;
-      for (int j = 0; j < sz; j++) {
-        ans.push_back(ans[j] * cur);
-      }
-    }
+    ans *= e + 1;
   }
   return ans;
 }
