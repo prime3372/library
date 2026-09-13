fbe17826e7da672e88d2b93bba4b89b3d5764e1d
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-14 01:00:51 +0900

    a

--- util/length_of_lis.hpp
+++ util/length_of_lis.hpp
@@ -1,22 +1,25 @@
 #pragma once
 
 #include <algorithm>
+#include <cassert>
+#include <functional>
 #include <limits>
 #include <vector>
 
 namespace cp {
 
-template <bool strong = true, class T>
-int length_of_lis(const std::vector<T>& a) {
+template <bool strong = true, class T, class Comp = std::less<T>>
+int length_of_lis(const std::vector<T>& a, Comp comp = Comp()) {
   int n = int(a.size());
   std::vector<T> dp(n, std::numeric_limits<T>::max());
   int ans = 0;
   for (int i = 0; i < n; i++) {
-    int j =
-        strong ? int(std::lower_bound(dp.begin(), dp.end(), a[i]) - dp.begin())
-               : int(std::upper_bound(dp.begin(), dp.end(), a[i]) - dp.begin());
-    dp[j] = a[i];
-    ans = std::max(ans, j + 1);
+    auto itr = strong ? std::lower_bound(dp.begin(), dp.end(), a[i], comp)
+                      : std::upper_bound(dp.begin(), dp.end(), a[i], comp);
+    int k = int(itr - dp.begin());
+    assert(k < n);
+    dp[k] = a[i];
+    ans = std::max(ans, k + 1);
   }
   return ans;
 }
