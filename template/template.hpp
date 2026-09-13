573dc27caf5295481c1fe7031f6b2c863c386538
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-14 01:21:17 +0900

    add macro

--- template/template.hpp
+++ template/template.hpp
@@ -26,7 +26,35 @@ template <class T> using maxque = std::priority_queue<T>;
 using namespace cp;
 using mint = cp::modint998244353;
 
-#ifdef LOCAL
+#define CHOOSE5(_1, _2, _3, _4, name, ...) name
+#define CHOOSE6(_1, _2, _3, _4, _5, name, ...) name
+
+#define rep1(n) for (ll _ = 0; _ < ll(n); _++)
+#define rep2(i, n) for (ll i = 0; i < ll(n); i++)
+#define rep3(i, a, b) for (ll i = (a); i < ll(b); i++)
+#define rep4(i, a, b, c) for (ll i = (a); i < ll(b); i += c)
+#define rep(...) CHOOSE5(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+
+#define REP2(i, n) for (ll i = 0; i <= ll(n); i++)
+#define REP3(i, a, b) for (ll i = (a); i <= ll(b); i++)
+#define REP4(i, a, b, c) for (ll i = (a); i <= ll(b); i += c)
+#define REP(...) CHOOSE5(__VA_ARGS__, REP4, REP3, REP2)(__VA_ARGS__)
+
+#define rrep2(i, n) for (ll i = ll(n) - 1; i >= 0; i--)
+#define rrep3(i, a, b) for (ll i = ll(b) - 1; i >= ll(a); i--)
+#define rrep4(i, a, b, c) for (ll i = ll(b) - 1; i >= ll(a); i -= c)
+#define rrep(...) CHOOSE5(__VA_ARGS__, rrep4, rrep3, rrep2)(__VA_ARGS__)
+
+#define fore1(x, a) for (auto&& x : (a))
+#define fore2(x, y, a) for (auto&& [x, y] : (a))
+#define fore3(x, y, z, a) for (auto&& [x, y, z] : (a))
+#define fore4(x, y, z, w, a) for (auto&& [x, y, z, w] : (a))
+#define fore(...) CHOOSE6(__VA_ARGS__, fore4, fore3, fore2, fore1)(__VA_ARGS__)
+
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+#define inrange(i, a, b) ((a) <= (i) && (i) < (b))
 
 #define GET_STRING_SINGLE(a) #a
 #define GET_STRING(...)                                \
@@ -35,7 +63,10 @@ using mint = cp::modint998244353;
     return _s.substr(1, _s.size() - 2);                \
   }()
 
+#ifdef LOCAL
+
 #define local 1
+
 #define dump(...)                                    \
   [](const auto&... _args) {                         \
     std::string _args_str = GET_STRING(__VA_ARGS__); \
@@ -56,16 +87,8 @@ using mint = cp::modint998244353;
 #else
 
 #define local 0
+
 #define dump(...)
 #define write(...)
 
 #endif
-
-#define rep(i, a, b) for (ll i = ll(a); i < ll(b); i++)
-#define REP(i, a, b) for (ll i = ll(a); i <= ll(b); i++)
-#define rrep(i, a, b) for (ll i = ll(b) - 1; i >= ll(a); i--)
-#define RREP(i, a, b) for (ll i = ll(b); i >= ll(a); i--)
-#define fore(x, a) for (auto&& x : (a))
-#define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
-#define inrange(i, a, b) ((a) <= (i) && (i) < (b))
