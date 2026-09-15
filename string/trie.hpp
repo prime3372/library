135761a1b68c99bc173bb7ec71bb7fc1c5019093
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-16 02:38:53 +0900

    delete `trie::prefix`

--- string/trie.hpp
+++ string/trie.hpp
@@ -55,15 +55,6 @@ template <int char_size, char offset = 'a'> class trie {
     return nodes[v].end;
   }
 
-  int prefix(const std::string& s) const {
-    int v = search(s);
-    return v == -1 ? 0 : prefix(v);
-  }
-  int prefix(int v) const {
-    assert(0 <= v && v < int(nodes.size()));
-    return nodes[v].mid;
-  }
-
   const std::array<int, char_size>& operator[](int v) const {
     assert(0 <= v && v < int(nodes.size()));
     return nodes[v].to;
