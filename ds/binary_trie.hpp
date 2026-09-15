327286cd5cde48cd3477da253390df5e0f4d1921
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-16 00:56:19 +0900

    fix

--- ds/binary_trie.hpp
+++ ds/binary_trie.hpp
@@ -18,7 +18,7 @@ class binary_trie : public trie<2, '0'> {
   int insert(T x) { return base::insert(std::bitset<bit>(x).to_string()); }
   bool erase(T x) { return base::erase(std::bitset<bit>(x).to_string()); }
   int count(T x) { return base::count(std::bitset<bit>(x).to_string()); }
-  int count_prefix(T x) {
+  int prefix(T x) {
     return base::count_prefix(std::bitset<bit>(x).to_string());
   }
 
