2b3fbd83dddf24d54d3d07f8251fccf4c7f78ec6
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-16 03:16:14 +0900

    a

--- test/yosupo/tree/rooted_tree_isomorphism_classification.test.cpp
+++ test/yosupo/tree/rooted_tree_isomorphism_classification.test.cpp
@@ -2,6 +2,7 @@
   "https://judge.yosupo.jp/problem/rooted_tree_isomorphism_classification"
 
 #include "tree/rooted_tree_hash.hpp"
+#include "util/algo_utility.hpp"
 #include <iostream>
 #include <vector>
 
@@ -19,16 +20,8 @@ int main() {
     cin >> p;
     t.add_edge(i, p);
   }
-  auto hs = t.build(0).hash;
-  vector<pair<hash61, int>> hs_idx(n);
-  for (int i = 0; i < n; i++) hs_idx[i] = {hs[i], i};
-  sort(hs_idx.begin(), hs_idx.end());
-  int id = 0;
-  vector<int> ans(n);
-  for (int i = 0; i < n; i++) {
-    ans[hs_idx[i].second] = id;
-    if (i < n - 1 && hs_idx[i].first != hs_idx[i + 1].first) id++;
-  }
-  cout << id + 1 << "\n";
+  auto hash = t.build(0).hash;
+  auto ans = compress(hash);
+  cout << max(ans) + 1 << "\n";
   for (int x : ans) cout << x << " ";
 }
