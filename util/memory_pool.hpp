d3d56c3d66cca8697f9b62d91f71c75ba39e3744
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-16 16:29:09 +0900

    a

--- util/memory_pool.hpp
+++ util/memory_pool.hpp
@@ -11,33 +11,26 @@ namespace cp {
 template <class T> class memory_pool {
  public:
   T* malloc() {
-    if (free_head) {
-      slot* ptr = free_head;
-      free_head = ptr->next;
-      return reinterpret_cast<T*>(ptr);
+    if (!free_list.empty()) {
+      T* ptr = free_list.back();
+      free_list.pop_back();
+      return ptr;
     }
     if (chunk_pos == chunk_size) {
-      chunks.push_back((slot*)(::operator new(chunk_size * sizeof(slot))));
+      chunks.push_back((T*)(::operator new(chunk_size * sizeof(T))));
       chunk_pos = 0;
     }
     return reinterpret_cast<T*>(chunks.back() + (chunk_pos++));
   }
 
   void free(T* x) {
-    slot* ptr = reinterpret_cast<slot*>(x);
-    ptr->next = free_head;
-    free_head = ptr;
+    if (x) free_list.push_back(x);
   }
 
  private:
-  union slot {
-    char data[sizeof(T)];
-    slot* next;
-  };
   static constexpr int chunk_size = 1 << 12;
-
-  inline static std::vector<slot*> chunks;
-  inline static slot* free_head = nullptr;
+  inline static std::vector<T*> chunks;
+  inline static std::vector<T*> free_list;
   inline static int chunk_pos = chunk_size;
 };
 
