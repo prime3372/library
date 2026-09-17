1a565b799d6207aa1497b20825edd5cbb40a95a1
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-17 19:05:07 +0900

    a

--- template/template.hpp
+++ template/template.hpp
@@ -23,7 +23,6 @@ using str = std::string;
 template <class T>
 using minque = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 template <class T> using maxque = std::priority_queue<T>;
-using std::views::elements;
 using std::views::keys;
 using std::views::values;
 #if __cplusplus >= 202302L
