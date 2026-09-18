cfc9c9c9fe46f5a23243f139b599c4e3410a2ea5
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-18 16:07:26 +0900

    create ds/persistent_array.hpp

---
+++ ds/persistent_array.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <array>
+#include <cassert>
+#include <utility>
+#include <vector>
+
+namespace cp {
+
+template <class T> class persistent_array {
+  using ull = unsigned long long;
+
+ public:
+  static constexpr int shift = 4;
+  static constexpr int mask = (1 << shift) - 1;
+  union node {
+   private:
+    friend persistent_array;
+    std::array<node*, 1 << shift> to;
+    T val;
+    node() { to.fill(nullptr); }
+    explicit node(const T& v) : val(v) {}
+  };
+
+  persistent_array() : n(0), depth(0), init_val() {}
+  explicit persistent_array(ull _n, T val = T())
+      : root(new node()), n(_n), depth(0), init_val(val) {
+    for (ull i = n; i; i >>= shift) depth++;
+  }
+
+  const T& operator[](ull k) const {
+    assert(k < n);
+    node* t = root;
+    for (int i = 0; t && i < depth; i++) {
+      t = t->to[k & mask];
+      k >>= shift;
+    }
+    return t ? t->val : init_val;
+  }
+
+  node* set(ull k, const T& val) {
+    assert(k < n);
+    node* t = root;
+    std::vector<std::pair<node*, int>> path(depth);
+    for (int i = 0; i < depth; i++) {
+      path[i] = {t, k & mask};
+      t = t ? t->to[k & mask] : nullptr;
+      k >>= shift;
+    }
+    node* cur = new node(val);
+    for (int i = depth - 1; i >= 0; i--) {
+      auto [par, j] = path[i];
+      node* nxt = par ? new node(*par) : new node();
+      nxt->to[j] = cur;
+      cur = nxt;
+    }
+    return root = cur;
+  }
+
+  node* get_root() { return root; }
+  void restore(node* new_root) { root = new_root; }
+
+ private:
+  node* root = nullptr;
+  ull n;
+  int depth;
+  T init_val;
+};
+
+}  // namespace cp
---
+++ test/yosupo/data_structure/persistent_queue.test.cpp
@@ -0,0 +1,42 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/persistent_queue"
+
+#include "ds/persistent_array.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace cp;
+
+int main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(nullptr);
+  int q;
+  cin >> q;
+  vector<int> l(q + 1), r(q + 1);
+  vector<persistent_array<int>::node*> past(q + 1);
+  persistent_array<int> s(q + 1);
+  past[0] = s.get_root();
+  l[0] = r[0] = 0;
+  for (int i = 1; i <= q; i++) {
+    int type;
+    cin >> type;
+    if (type == 0) {
+      int t, x;
+      cin >> t >> x;
+      t++;
+      s.restore(past[t]);
+      past[i] = s.set(r[t], x);
+      l[i] = l[t];
+      r[i] = r[t] + 1;
+    } else {
+      int t;
+      cin >> t;
+      t++;
+      s.restore(past[t]);
+      cout << s[l[t]] << "\n";
+      past[i] = past[t];
+      l[i] = l[t] + 1;
+      r[i] = r[t];
+    }
+  }
+}
