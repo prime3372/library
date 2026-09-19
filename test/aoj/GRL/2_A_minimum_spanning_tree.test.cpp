fcee59faf54edb269f708c145e0b94f80cff7fcc
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-20 06:00:41 +0900

    a

--- test/aoj/GRL/2_A_minimum_spanning_tree.test.cpp
+++ test/aoj/GRL/2_A_minimum_spanning_tree.test.cpp
@@ -2,7 +2,9 @@
   "https://onlinejudge.u-aizu.ac.jp/courses/library/3/GRL/all/GRL_2_A"
 
 #include "graph/kruscal.hpp"
+#include <cassert>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 using namespace cp;
@@ -13,10 +15,16 @@ int main() {
   int n, m;
   cin >> n >> m;
   kruscal<int> g(n);
+  std::vector<int> weight;
   for (int i = 0; i < m; i++) {
     int s, t, w;
     cin >> s >> t >> w;
     g.add_edge(s, t, w);
+    weight.push_back(w);
   }
-  cout << g.min() << "\n";
+  int ans = g.min();
+  int w_sum = 0;
+  for (int e : g.mst()) w_sum += weight[e];
+  assert(w_sum == ans);
+  cout << ans << "\n";
 }
