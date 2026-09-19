8b1c2e71b926494183c91b3e5e47a962a6d48dc3
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-20 07:24:52 +0900

    a

--- test/unittest/random_graph.test.cpp
+++ test/unittest/random_graph.test.cpp
@@ -22,8 +22,8 @@ void complete() {
   for (auto [u, v] : edges) {
     assert(1 <= u && u <= n);
     assert(1 <= v && v <= n);
-    u--, v--;
     if (u > v) swap(u, v);
+    u--, v--;
     assert(!exist[u][v]);
     exist[u][v] = true;
   }
@@ -38,7 +38,6 @@ void undirected_simple_connected() {
     assert(1 <= u && u <= n);
     assert(1 <= v && v <= n);
     assert(u != v);
-    u--, v--;
     if (u > v) swap(u, v);
   }
   sort(edges.begin(), edges.end());
@@ -46,8 +45,24 @@ void undirected_simple_connected() {
     assert(edges[i] != edges[i + 1]);
   }
   union_find uf(n);
-  for (auto [u, v] : edges) uf.unite(u, v);
+  for (auto [u, v] : edges) uf.unite(u - 1, v - 1);
   assert(uf.groups().size() == 1);
+}
+
+void directed_simple() {
+  int n = uniform(1, 100);
+  int m = uniform(n - 1, n * (n - 1) / 2);
+  auto edges = random_graph<false, true, true, false>(n, m);
+  assert(int(edges.size()) == m);
+  for (auto& [u, v] : edges) {
+    assert(1 <= u && u <= n);
+    assert(1 <= v && v <= n);
+    assert(u != v);
+  }
+  sort(edges.begin(), edges.end());
+  for (int i = 0; i < int(edges.size()) - 1; i++) {
+    assert(edges[i] != edges[i + 1]);
+  }
 }
 
 void reachable_from_s() {
@@ -78,6 +93,7 @@ void reachable_from_s() {
 int main() {
   for (int i = 0; i < 1000; i++) complete();
   for (int i = 0; i < 1000; i++) undirected_simple_connected();
+  for (int i = 0; i < 1000; i++) directed_simple();
   for (int i = 0; i < 1000; i++) reachable_from_s();
   cout << "Hello World\n";
 }
