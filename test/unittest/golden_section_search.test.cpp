002cfa5081220841d9c6ab8f9a45a728e9bfd0fd
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-19 20:25:24 +0900

    create golden_section_search.hpp

---
+++ test/unittest/golden_section_search.test.cpp
@@ -0,0 +1,70 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A"
+
+#include "random/engine.hpp"
+#include "util/golden_section_search.hpp"
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+using namespace cp;
+using ll = long long;
+
+int max_n = 10000;
+ll max_a = 1000000000000000000LL;
+
+void random() {
+  int n = uniform(1, max_n);
+  vector<ll> a(n);
+  for (ll& x : a) x = uniform(0LL, max_a);
+  sort(a.begin(), a.end());
+  a.erase(unique(a.begin(), a.end()), a.end());
+  n = int(a.size());
+  int x = uniform(0, n - 1);
+  std::reverse(a.end() - x, a.end());
+  std::rotate(a.begin(), a.end() - x, a.end());
+  for (int i = 0; i < x; i++) assert(a[i] > a[i + 1]);
+  for (int i = x; i < n - 1; i++) assert(a[i] < a[i + 1]);
+  auto f = [&](ll i) -> ll { return a[i]; };
+  auto ans = golden_section_search(f, 0, n - 1);
+  assert(ans.first == x);
+  assert(ans.second == a[x]);
+}
+
+void decreasing() {
+  int n = uniform(1, max_n);
+  vector<ll> a(n);
+  for (ll& x : a) x = uniform(0LL, max_a);
+  sort(a.rbegin(), a.rend());
+  a.erase(unique(a.begin(), a.end()), a.end());
+  n = int(a.size());
+  for (int i = 0; i < n - 1; i++) assert(a[i] > a[i + 1]);
+  auto f = [&](ll i) -> ll { return a[i]; };
+  auto ans = golden_section_search(f, 0, n - 1);
+  assert(ans.first == n - 1);
+  assert(ans.second = a[n - 1]);
+}
+
+void increasing() {
+  int n = uniform(1, max_n);
+  vector<ll> a(n);
+  for (ll& x : a) x = uniform(0LL, max_a);
+  sort(a.begin(), a.end());
+  a.erase(unique(a.begin(), a.end()), a.end());
+  n = int(a.size());
+  for (int i = 0; i < n - 1; i++) assert(a[i] < a[i + 1]);
+  auto f = [&](ll i) -> ll { return a[i]; };
+  auto ans = golden_section_search(f, 0, n - 1);
+  assert(ans.first == 0);
+  assert(ans.second = a[0]);
+}
+
+int main() {
+  for (int i = 0; i < 100; i++) random();
+  for (int i = 0; i < 10; i++) decreasing();
+  for (int i = 0; i < 10; i++) increasing();
+  cout << "Hello World\n";
+}
---
+++ util/golden_section_search.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cassert>
+#include <functional>
+#include <type_traits>
+#include <utility>
+
+namespace cp {
+
+// [l, r]
+template <bool get_min = true, class F,
+          class T = std::invoke_result_t<F, long long>>
+std::pair<long long, T> golden_section_search(F f, long long l, long long r) {
+  assert(l <= r);
+  long long a = l - 1, s = 1, t = 2;
+  while (t < r - l + 2) std::swap(s += t, t);
+  long long x = a + t - s, b = a + t;
+  T fx = f(x), fy;
+  while (a + b != 2 * x) {
+    long long y = a + b - x;
+    if (r < y || (fy = f(y), get_min ? fx < fy : fx > fy)) {
+      b = a;
+      a = y;
+    } else {
+      a = x;
+      x = y;
+      fx = fy;
+    }
+  }
+  return {x, fx};
+}
+
+}  // namespace cp
