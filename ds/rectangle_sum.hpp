861528b500f95d6db6f440feaef87638f8f4a15e
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-19 18:37:37 +0900

    set bit_size to 30

--- ds/rectangle_sum.hpp
+++ ds/rectangle_sum.hpp
@@ -54,7 +54,7 @@ template <class T, class U> class rectangle_sum {
   }
 
  private:
-  static constexpr int bit_size = 32;
+  static constexpr int bit_size = 30;
   int n = 0;
   std::vector<T> xs, ys;
   std::vector<U> ws;
