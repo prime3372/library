280d1a3cb2746577a479348f4ee25c1c07b9c936
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-20 04:46:48 +0900

    a

--- test/unittest/dynamic_bitset.test.cpp
+++ test/unittest/dynamic_bitset.test.cpp
@@ -13,7 +13,7 @@ using namespace std;
 using namespace cp;
 using ll = long long;
 
-template <int k> void random(int n) {
+template <int k> void test(int n) {
   dynamic_bitset bs1(k);
   std::bitset<k> bs2;
   for (int i = 0; i < k; i++) bs1[i] = bs2[i] = uniform_bool();
@@ -37,21 +37,14 @@ template <int k> void random(int n) {
     assert(bs1.count() == int(bs2.count()));
   }
 }
-void width_1() { random<1>(500000); }
-void width_10() { random<10>(500000); }
-void width_255() { random<255>(500000); }
-void width_256() { random<256>(500000); }
-void width_257() { random<257>(500000); }
-void width_1000() { random<1000>(10000); }
-void width_1000000() { random<1000000>(10); }
 
 int main() {
-  width_1();
-  width_10();
-  width_255();
-  width_256();
-  width_257();
-  width_1000();
-  width_1000000();
+  test<1>(500000);
+  test<10>(500000);
+  test<255>(500000);
+  test<256>(500000);
+  test<257>(500000);
+  test<1000>(10000);
+  test<1000000>(10);
   cout << "Hello World\n";
 }
