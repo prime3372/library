4bf1a509e1e00eab6c26456ba980250766d28e7f
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-19 16:19:49 +0900

    refactor wavelet_matrix

--- ds/wavelet_matrix.hpp
+++ ds/wavelet_matrix.hpp
@@ -2,77 +2,35 @@
 
 #include <algorithm>
 #include <array>
-#include <bit>
+#include <bitset>
 #include <cassert>
 #include <limits>
 #include <utility>
 #include <vector>
 
+#include "ds/bit_vector.hpp"
 #include "util/type_traits.hpp"
 
 namespace cp {
 
-template <class T> requires(internal::is_unsigned_int_v<T>)
-class wavelet_matrix {
+template <int bit_size> class wavelet_matrix {
+  using bs = std::bitset<bit_size>;
+
  public:
-  class bit_vector {
-   public:
-    bit_vector() {}
-    explicit bit_vector(int _n) : n(_n), block(n / 64 + 1), count(n / 64 + 1) {}
-
-    void set(int i) {
-      assert(0 <= i && i < n);
-      block[i / 64] |= 1LL << (i % 64);
-    }
-
-    void build() {
-      for (int i = 0; i < int(block.size()) - 1; i++) {
-        count[i + 1] = count[i] + std::popcount(block[i]);
-      }
-    }
-
-    bool operator[](int i) const {
-      assert(0 <= i && i < n);
-      return (block[i / 64] & mask(i % 64)) != 0;
-    }
-
-    int rank0(int i) const {
-      assert(0 <= i && i <= n);
-      return i - rank1(i);
-    }
-    int rank1(int i) const {
-      assert(0 <= i && i <= n);
-      return count[i / 64] + std::popcount(block[i / 64] & (mask(i % 64) - 1));
-    }
-
-    int next0(int i) const { return rank0(i); }
-    int next1(int i) const { return rank0(n) + rank1(i); }
-    int next(int i) const { return (*this)[i] ? next1(i) : next0(i); }
-
-   private:
-    int n;
-    std::vector<unsigned long long> block;
-    std::vector<int> count;
-
-    static unsigned long long mask(int pos) { return 1ULL << pos; }
-  };
-
-  static constexpr int digits() { return w; }
-
   wavelet_matrix() : wavelet_matrix(0) {}
   explicit wavelet_matrix(int _n) : n(_n), a(_n) {}
 
-  void set(int i, T x) {
+  void set(int i, const bs& x) {
     assert(0 <= i && i < n);
     a[i] = x;
   }
 
   void build() {
-    data.assign(w, bit_vector(n));
-    std::vector<T> cur = a, nxt(n);
-    for (int h = w - 1; h >= 0; h--) {
+    data.assign(bit_size, bit_vector(n));
+    std::vector<bs> cur = a, nxt(n);
+    for (int h = bit_size - 1; h >= 0; h--) {
       for (int i = 0; i < n; i++) {
-        if ((cur[i] >> h) & 1) data[h].set(i);
+        if (cur[i][h]) data[h].set(i);
       }
       data[h].build();
       std::array itr = {nxt.begin(), nxt.begin() + data[h].rank0(n)};
@@ -82,14 +40,29 @@ class wavelet_matrix {
   }
 
   const bit_vector& operator[](int h) const {
-    assert(0 <= h && h < w);
+    assert(0 <= h && h < bit_size);
     return data[h];
   }
 
+  int next0(int h, int i) const {
+    assert(0 <= h && h < bit_size);
+    assert(0 <= i && i <= n);
+    return data[h].rank0(i);
+  }
+  int next1(int h, int i) const {
+    assert(0 <= h && h < bit_size);
+    assert(0 <= i && i <= n);
+    return data[h].rank0(n) + data[h].rank1(i);
+  }
+  int next(int h, int i) const {
+    assert(0 <= h && h < bit_size);
+    assert(0 <= i && i < n);
+    return data[h][i] ? next1(h, i) : next0(h, i);
+  }
+
  private:
-  static constexpr int w = std::numeric_limits<T>::digits;
   int n;
-  std::vector<T> a;
+  std::vector<bs> a;
   std::vector<bit_vector> data;
 };
 
