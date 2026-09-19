69db7aaec0c1887dc3c2b1ca232ee706b1b04bef
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-19 17:42:19 +0900

    create rectangle_sum.hpp

---
+++ ds/rectangle_sum.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+#include "ds/wavelet_matrix.hpp"
+#include "util/algo_utility.hpp"
+#include "util/coordinate_compression.hpp"
+
+namespace cp {
+
+template <class T, class U> class rectangle_sum {
+ public:
+  rectangle_sum() {}
+
+  void add_point(T x, T y, U w) {
+    xs.push_back(x);
+    ys.push_back(y);
+    ws.push_back(w);
+    n++;
+  }
+
+  void init() {
+    auto p = sort(xs);
+
+    std::vector<int> comp(n);
+    {
+      auto q = sort(ys);
+      for (int i = 0; i < n; i++) comp[q[i]] = i;
+    }
+    wm = wavelet_matrix<bit_size>(n);
+    for (int i = 0; i < n; i++) wm.set(i, comp[p[i]]);
+    wm.init();
+
+    cum.assign(bit_size, std::vector<U>(n + 1));
+    for (int i = 0; i < n; i++) {
+      for (int k = i, h = bit_size - 1; h >= 0; h--) {
+        k = wm.next(h, k);
+        cum[h][k + 1] = ws[p[i]];
+      }
+    }
+    for (int h = 0; h < bit_size; h++) {
+      for (int i = 0; i < n; i++) {
+        cum[h][i + 1] += cum[h][i];
+      }
+    }
+    initialized = true;
+  }
+
+  U query(T l, T d, T r, T u) {
+    assert(initialized);
+    return prefix_sum(l, r, u) - prefix_sum(l, r, d);
+  }
+
+ private:
+  static constexpr int bit_size = 32;
+  int n = 0;
+  std::vector<T> xs, ys;
+  std::vector<U> ws;
+  wavelet_matrix<bit_size> wm;
+  std::vector<std::vector<U>> cum;
+  bool initialized = false;
+
+  U prefix_sum(T l, T r, T u) {
+    int left = int(std::lower_bound(xs.begin(), xs.end(), l) - xs.begin());
+    int right = int(std::lower_bound(xs.begin(), xs.end(), r) - xs.begin());
+    int upper = int(std::lower_bound(ys.begin(), ys.end(), u) - ys.begin());
+    U ans = 0;
+    for (int h = bit_size - 1; h >= 0; h--) {
+      int l0 = wm.next0(h, left);
+      int r0 = wm.next0(h, right);
+      if ((upper >> h) & 1) {
+        ans += cum[h][r0] - cum[h][l0];
+        left = wm.next1(h, left);
+        right = wm.next1(h, right);
+      } else {
+        left = l0;
+        right = r0;
+      }
+    }
+    return ans;
+  }
+};
+
+}  // namespace cp
--- test/yosupo/data_structure/rectangle_sum.test.cpp
+++ test/yosupo/data_structure/rectangle_sum.test.cpp
@@ -1,8 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/rectangle_sum"
 
-#include "ds/wavelet_matrix.hpp"
-#include "util/algo_utility.hpp"
-#include "util/coordinate_compression.hpp"
+#include "ds/rectangle_sum.hpp"
 #include <iostream>
 #include <tuple>
 #include <vector>
@@ -12,56 +10,21 @@ using namespace cp;
 using ll = long long;
 
 int main() {
-  constexpr int bit_size = 32;
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   int n, q;
   cin >> n >> q;
-  vector<unsigned int> x(n), y(n);
-  vector<ll> w(n);
-  for (int i = 0; i < n; i++) cin >> x[i] >> y[i] >> w[i];
-
-  auto p = sort(x);
-  wavelet_matrix<unsigned int> wm(n);
-  for (int i = 0; i < n; i++) wm.set(i, y[p[i]]);
-  wm.build();
-
-  vector<vector<ll>> cum(bit_size, vector<ll>(n + 1));
+  rectangle_sum<int, ll> sum;
   for (int i = 0; i < n; i++) {
-    int k = i;
-    for (int h = bit_size - 1; h >= 0; h--) {
-      k = wm[h].next(k);
-      cum[h][k + 1] = w[p[i]];
-    }
+    int x, y;
+    ll w;
+    cin >> x >> y >> w;
+    sum.add_point(x, y, w);
   }
-  for (int h = 0; h < bit_size; h++) {
-    for (int i = 0; i < n; i++) {
-      cum[h][i + 1] += cum[h][i];
-    }
-  }
-
-  auto sum = [&](int l, int r, unsigned int u) -> ll {
-    ll ans = 0;
-    for (int h = bit_size - 1; h >= 0; h--) {
-      int l0 = wm[h].next0(l);
-      int r0 = wm[h].next0(r);
-      if ((u >> h) & 1) {
-        ans += cum[h][r0] - cum[h][l0];
-        l = wm[h].next1(l);
-        r = wm[h].next1(r);
-      } else {
-        l = l0;
-        r = r0;
-      }
-    }
-    return ans;
-  };
-
+  sum.init();
   while (q--) {
-    unsigned int l, d, r, u;
+    int l, d, r, u;
     cin >> l >> d >> r >> u;
-    int l_lb = int(std::lower_bound(x.begin(), x.end(), l) - x.begin());
-    int r_lb = int(std::lower_bound(x.begin(), x.end(), r) - x.begin());
-    cout << sum(l_lb, r_lb, u) - sum(l_lb, r_lb, d) << "\n";
+    cout << sum.query(l, d, r, u) << "\n";
   }
 }
