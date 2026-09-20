f4d2e63cfdf94c64476c0adc62139eff7c023aa6
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-21 05:38:38 +0900

    fix

--- graph/two_edge_connected_components.hpp
+++ graph/two_edge_connected_components.hpp
@@ -9,7 +9,8 @@ namespace cp {
 
 class two_edge_connected_components {
  public:
-  two_edge_connected_components(int _n) : id(_n, -1), n(_n), link(_n) {}
+  two_edge_connected_components() : n(0) {}
+  explicit two_edge_connected_components(int _n) : id(_n), n(_n), link(_n) {}
 
   void add_edge(int a, int b) {
     assert(0 <= a && a < n);
@@ -17,30 +18,25 @@ class two_edge_connected_components {
     link.add_edge(a, b);
   }
 
+  int group_num = -1;
   std::vector<int> id;
   std::vector<std::vector<int>> groups;
 
   two_edge_connected_components& build() {
+    group_num = 0;
+    std::fill(id.begin(), id.end(), -1);
     link.build();
-
-    int group_num = 0;
-    auto dfs = [&](auto self, int v, int pv, int& k) -> void {
-      if (pv != -1 && link.ord[pv] >= link.low[v]) {
-        id[v] = id[pv];
-      } else {
-        id[v] = k++;
-      }
+    auto dfs = [&](auto self, int v, int pv) -> void {
+      id[v] = (pv == -1 || link.ord[pv] < link.low[v] ? group_num++ : id[pv]);
       for (auto& e : link.g[v]) {
-        if (id[e.to] == -1) self(self, e.to, v, k);
+        if (id[e.to] == -1) self(self, e.to, v);
       }
     };
     for (int i = 0; i < n; i++) {
-      if (id[i] == -1) dfs(dfs, i, -1, group_num);
+      if (id[i] == -1) dfs(dfs, i, -1);
     }
-
-    groups.resize(group_num);
+    groups.assign(group_num, {});
     for (int i = 0; i < n; i++) groups[id[i]].push_back(i);
-
     return *this;
   }
 
