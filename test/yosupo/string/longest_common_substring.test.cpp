cbf8e18080550f56153a368bccafca889acf2cda
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-20 22:51:30 +0900

    a

--- test/yosupo/string/longest_common_substring.test.cpp
+++ test/yosupo/string/longest_common_substring.test.cpp
@@ -18,21 +18,21 @@ int main() {
   s.append(t);
   auto sa = suffix_array(s);
   auto lcp = lcp_array(s, sa);
-  int max = 0, idx = -1;
+  int max = 0, p = -1;
   for (int i = 0; i < n + m + 1; i++) {
     if ((sa[i] < n) == (sa[i + 1] < n)) continue;
     if (lcp[i] > max) {
       max = lcp[i];
-      idx = i;
+      p = i;
     }
   }
   if (max == 0) {
     cout << 0 << " " << 0 << " " << 0 << " " << 0 << "\n";
-  } else if (sa[idx] < n) {
-    cout << sa[idx] << " " << sa[idx] + max << " ";
-    cout << sa[idx + 1] - n - 1 << " " << sa[idx + 1] + max - n - 1 << "\n";
+  } else if (sa[p] < n) {
+    cout << sa[p] << " " << sa[p] + max << " ";
+    cout << sa[p + 1] - n - 1 << " " << sa[p + 1] + max - n - 1 << "\n";
   } else {
-    cout << sa[idx + 1] << " " << sa[idx + 1] + max << " ";
-    cout << sa[idx] - n - 1 << " " << sa[idx] + max - n - 1 << "\n";
+    cout << sa[p + 1] << " " << sa[p + 1] + max << " ";
+    cout << sa[p] - n - 1 << " " << sa[p] + max - n - 1 << "\n";
   }
 }
