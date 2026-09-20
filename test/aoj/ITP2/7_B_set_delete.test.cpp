917aa75ba31168b05d5b47f53d3f1e895c981113
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-21 05:47:49 +0900

    add a test for `hash_set`

---
+++ test/aoj/ITP2/7_B_set_delete.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/all/ITP2_7_C"
+
+#include "ds/hash_set.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cp;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int q;
+  cin >> q;
+  hash_set<int> s;
+  while (q--) {
+    int t;
+    cin >> t;
+    if (t == 0) {
+      int x;
+      cin >> x;
+      s.insert(x);
+      cout << s.size() << "\n";
+    } else if (t == 1) {
+      int x;
+      cin >> x;
+      cout << s.count(x) << "\n";
+    } else if (t == 2) {
+      int x;
+      cin >> x;
+      s.erase(x);
+    }
+  }
+}
