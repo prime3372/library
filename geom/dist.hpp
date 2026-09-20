23c7c58412c58e72992236c4de262af94f50f2ca
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-20 23:52:05 +0900

    refactor geometry

--- geom/dist.hpp
+++ geom/dist.hpp
@@ -1,5 +1,58 @@
 #pragma once
 
-#include "dist_pl.hpp"
-#include "dist_ps.hpp"
-#include "dist_ss.hpp"
+#include "geom/intersect.hpp"
+#include "geom/point.hpp"
+#include "geom/projection.hpp"
+#include "geom/segment.hpp"
+
+namespace cp {
+
+// point-line
+
+long double dist(const point<long double>& p, const line<long double>& l) {
+  return std::abs(cross(p - l.p, l.q - l.p)) / abs(l.q - l.p);
+}
+
+template <class T, class U>
+long double dist(const point<T>& p, const line<U>& l) {
+  return dist(point<long double>(p), line<long double>(l));
+}
+
+// point-segment
+
+long double dist(const point<long double>& p, const segment<long double>& s) {
+  point<long double> h = projection(p, line<long double>(s));
+  if (on(h, s)) return abs(h - p);
+  return std::min(abs(s.p - p), abs(s.q - p));
+}
+
+template <class T, class U>
+long double dist(const point<T>& p, const segment<U>& s) {
+  return dist(point<long double>(p), segment<long double>(s));
+}
+
+// line-segment
+
+long double dist(const line<long double>& l, const segment<long double>& s) {
+  if (intersect(l, s)) return 0;
+  return std::min(dist(s.p, l), dist(s.q, l));
+}
+
+template <class T, class U>
+long double dist(const line<T>& l, const segment<U>& s) {
+  return dist(line<long double>(l), segment<long double>(s));
+}
+
+// segment-segment
+
+long double dist(const segment<long double>& s, const segment<long double>& t) {
+  if (intersect(s, t)) return 0;
+  return std::min({dist(s.p, t), dist(s.q, t), dist(t.p, s), dist(t.q, s)});
+}
+
+template <class T, class U>
+long double dist(const segment<T> s, const segment<U>& t) {
+  return dist(segment<long double>(s), segment<long double>(t));
+}
+
+}  // namespace cp
--- geom/intersect.hpp
+++ geom/intersect.hpp
@@ -1,5 +1,23 @@
 #pragma once
 
-#include "geom/intersect_ll.hpp"
-#include "geom/intersect_ls.hpp"
-#include "geom/intersect_ss.hpp"
+#include "geom/ccw.hpp"
+#include "geom/line.hpp"
+#include "geom/segment.hpp"
+
+namespace cp {
+
+template <class T> bool intersect(const line<T>& l, const line<T>& m) {
+  return !is_parallel(l, m) || on(l.p, m);
+}
+
+template <class T> bool intersect(const line<T>& l, const segment<T>& s) {
+  return ccw(l.p, l.q, s.p) * ccw(l.p, l.q, s.q) != 1;
+}
+
+template <class T> bool intersect(const segment<T>& s, const segment<T>& t) {
+  int a = ccw(s.p, s.q, t.p) * ccw(s.p, s.q, t.q);
+  int b = ccw(t.p, t.q, s.p) * ccw(t.p, t.q, s.q);
+  return a == 0 || b == 0 || (a == -1 && b == -1);
+}
+
+}  // namespace cp
