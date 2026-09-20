a9926da864c76e998fa80192397b3c0f7c84ae71
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-20 22:48:42 +0900

    create string/manacher.hpp

---
+++ string/manacher.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace cp {
+
+std::vector<int> manacher(const std::string& s) {
+  int n = int(s.size());
+  std::string t(2 * n + 1, -1);
+  for (int i = 0; i < n; i++) t[2 * i + 1] = s[i];
+  n = 2 * n + 1;
+  std::vector<int> rad(n);
+  int c = 0, r = 0;
+  while (c < n) {
+    while (0 <= c - r && c + r < n && t[c - r] == t[c + r]) r++;
+    rad[c] = r;
+    int k = 1;
+    while (0 <= c - k && k + rad[c - k] < r) {
+      rad[c + k] = rad[c - k];
+      k++;
+    }
+    c += k;
+    r -= k;
+  }
+  std::vector<int> ans(n - 2);
+  for (int i = 0; i < n - 2; i++) ans[i] = rad[i + 1] - 1;
+  return ans;
+}
+
+}  // namespace cp
--- test/yosupo/string/enumerate_palindromes.test.cpp
+++ test/yosupo/string/enumerate_palindromes.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/enumerate_palindromes"
 
-#include "string/rolling_hash.hpp"
+#include "string/manacher.hpp"
 #include <iostream>
 
 using namespace std;
@@ -11,24 +11,6 @@ int main() {
   cin.tie(nullptr);
   string s;
   cin >> s;
-  rolling_hash rlh(s);
-  int n = int(s.size());
-  reverse(s.begin(), s.end());
-  rolling_hash rrlh(s);
-  for (int i = 0; i < 2 * n - 1; i++) {
-    int l = 0, r = n;
-    while (r - l > 1) {
-      int mid = (l + r) / 2, p = (i + 1) / 2, q = p + (i % 2 == 0);
-      if (p - mid < 0 || n - q - mid < 0) {
-        r = mid;
-        continue;
-      }
-      if (rlh.get(p - mid, p) == rrlh.get(n - q - mid, n - q)) {
-        l = mid;
-      } else {
-        r = mid;
-      }
-    }
-    cout << 2 * l + (i % 2 == 0) << " ";
-  }
+  auto ans = manacher(s);
+  for (int x : ans) cout << x << " ";
 }
