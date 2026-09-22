4798c11306c4571bd254383132ab2c5ed6be097e
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-23 00:54:59 +0900

    refactor

--- ds/dynamic_segtree.hpp
+++ ds/dynamic_segtree.hpp
@@ -71,12 +71,29 @@ template <class S, auto op, auto e> class dynamic_segtree {
 
   void set(ull i, const S& x) {
     assert(i < n);
-    set(root, 0, sz, log, i, x);
+    node* t = root;
+    std::vector<node*> ps(log);
+    for (int h = log - 1; t && h >= 0; h--) {
+      ps[h] = t;
+      t = ((i >> h) & 1) ? t->right : t->left;
+    }
+    node* cur = t ? (t->val = x, t) : new node(x);
+    for (int h = 0; h < log; h++) {
+      node* nxt = ps[h] ? ps[h] : new node();
+      ((i >> h) & 1 ? nxt->right : nxt->left) = cur;
+      update(nxt, h + 1);
+      cur = nxt;
+    }
+    root = cur;
   }
 
   S operator[](ull i) const {
     assert(i < n);
-    return get(root, 0, sz, log, i);
+    node* t = root;
+    for (int h = log - 1; t && h >= 0; h--) {
+      t = ((i >> h) & 1) ? t->right : t->left;
+    }
+    return t ? t->val : initial_vals[0];
   }
 
   S prod(ull l, ull r) const {
@@ -114,6 +131,7 @@ template <class S, auto op, auto e> class dynamic_segtree {
     S val;
     node* left = nullptr;
     node* right = nullptr;
+    node() : val(e()) {}
     node(const S& x) : val(x) {}
   }* root = nullptr;
   ull n, sz;
@@ -123,32 +141,6 @@ template <class S, auto op, auto e> class dynamic_segtree {
   void update(node* t, int h) {
     t->val = op(t->left ? t->left->val : initial_vals[h - 1],
                 t->right ? t->right->val : initial_vals[h - 1]);
-  }
-
-  void set(node*& t, ull a, ull b, int h, ull i, const S& x) {
-    if (!t) t = new node(initial_vals[h]);
-    if (b - a == 1) {
-      t->val = x;
-      return;
-    }
-    ull c = (a + b) / 2;
-    if (i < c) {
-      set(t->left, a, c, h - 1, i, x);
-    } else {
-      set(t->right, c, b, h - 1, i, x);
-    }
-    update(t, h);
-  }
-
-  S get(const node* t, ull a, ull b, int h, ull i) const {
-    if (!t) return initial_vals[0];
-    if (b - a == 1) return t->val;
-    ull c = (a + b) / 2;
-    if (i < c) {
-      return get(t->left, a, c, h - 1, i);
-    } else {
-      return get(t->right, c, b, h - 1, i);
-    }
   }
 
   S prod(const node* t, ull a, ull b, int h, ull l, ull r) const {
