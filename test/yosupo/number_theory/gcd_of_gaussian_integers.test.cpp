edc49402a801c1de9cb6c9e026507305ec5456b3
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-24 02:27:45 +0900

    merge gcd_of_gaussian_integers.hpp into gcd_of_gaussian_integers.test.cpp

--- test/yosupo/number_theory/gcd_of_gaussian_integers.test.cpp
+++ test/yosupo/number_theory/gcd_of_gaussian_integers.test.cpp
@@ -1,12 +1,16 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/gcd_of_gaussian_integers"
 
-#include "number/gcd_of_gaussian_integers.hpp"
+#include "number/gaussian_integer.hpp"
 #include <iostream>
 
 using namespace std;
 using namespace cp;
 using ll = long long;
 using gint = gaussian_integer;
+
+gaussian_integer gcd(const gaussian_integer& z1, const gaussian_integer& z2) {
+  return (z2.x == 0 && z2.y == 0) ? z1 : gcd(z2, z1 % z2);
+}
 
 int main() {
   int t;
