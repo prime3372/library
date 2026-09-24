11dc5e62e0f05c3455d785f5bb940dd4ad431b4d
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-25 08:26:20 +0900

    check a.size() == b.size()

--- number/gcd_convolution.hpp
+++ number/gcd_convolution.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <vector>
 
 #include "number/multiple_zeta_mobius.hpp"
@@ -8,9 +9,9 @@ namespace cp {
 
 template <class T>
 std::vector<T> gcd_convolution(std::vector<T> a, std::vector<T> b) {
+  assert(a.size() == b.size());
   multiple_zeta(a);
   multiple_zeta(b);
-  if (a.size() < b.size()) a.resize(b.size());
   for (int i = 0; i < int(a.size()); i++) a[i] *= b[i];
   multiple_mobius(a);
   return a;
--- number/lcm_convolution.hpp
+++ number/lcm_convolution.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <vector>
 
 #include "number/divisor_zeta_mobius.hpp"
@@ -8,9 +9,9 @@ namespace cp {
 
 template <class T>
 std::vector<T> lcm_convolution(std::vector<T> a, std::vector<T> b) {
+  assert(a.size() == b.size());
   divisor_zeta(a);
   divisor_zeta(b);
-  if (a.size() < b.size()) a.resize(b.size());
   for (int i = 0; i < int(a.size()); i++) a[i] *= b[i];
   divisor_mobius(a);
   return a;
