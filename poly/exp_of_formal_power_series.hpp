27debbc8d3265af293671d57a3b235297f3ef9f6
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-25 05:32:20 +0900

    a

--- poly/exp_of_formal_power_series.hpp
+++ poly/exp_of_formal_power_series.hpp
@@ -13,11 +13,11 @@ formal_power_series<mint> exp(const formal_power_series<mint>& f, int n) {
   for (int k = 1; k < n; k *= 2) {
     auto z = g.prefix(2 * k);
     internal::ntt(z);
-    auto h = log(g, 2 * k) - f.prefix(2 * k);
+    auto h = f.prefix(2 * k) - log(g, 2 * k);
     internal::ntt(h);
     for (int i = 0; i < 2 * k; i++) h[i] *= z[i];
     internal::intt(h);
-    for (int i = k; i < std::min(2 * k, n); i++) g.emplace_back(-h[i]);
+    for (int i = k; i < std::min(2 * k, n); i++) g.emplace_back(h[i]);
   }
   return g;
 }
