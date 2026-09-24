403f42c5a196b407cc437323b7fa122e839f121a
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-24 14:04:26 +0900

    fix

--- comb/monmort.hpp
+++ comb/monmort.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <cassert>
 #include <vector>
 
 namespace cp {
 
 template <class T> std::vector<T> monmort(int n) {
+  assert(0 <= n);
+  if (n == 0) return {1};
   std::vector<T> a(n + 1);
   a[0] = 1;
   a[1] = 0;
