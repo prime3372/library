09f2bade241606c8aedf553f6b5ceeabc7943375
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-25 05:51:37 +0900

    use reserve to avoid reallocation

--- number/enumerate_divisors.hpp
+++ number/enumerate_divisors.hpp
@@ -12,8 +12,12 @@ namespace cp {
 // @param n `1 <= n`
 std::vector<long long> enumerate_divisors(long long n) {
   assert(1 <= n);
+  auto f = factorize(n);
+  long long cnt = 1;
+  for (auto [p, e] : f) cnt *= e + 1;
   std::vector<long long> ans = {1};
-  for (auto [p, e] : factorize(n)) {
+  ans.reserve(cnt);
+  for (auto [p, e] : f) {
     int sz = int(ans.size());
     long long cur = 1;
     for (int i = 0; i < e; i++) {
