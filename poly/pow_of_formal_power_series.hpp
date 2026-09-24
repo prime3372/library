1ce457e6dce5e353c6dadb715f26d848bd6b4076
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-25 03:09:20 +0900

    a

--- poly/pow_of_formal_power_series.hpp
+++ poly/pow_of_formal_power_series.hpp
@@ -11,7 +11,7 @@ formal_power_series<mint> pow(const formal_power_series<mint>& f, long long k,
   if (k == 0) return formal_power_series<mint>{1}.prefix(n);
   for (int i = 0; i * k < n; i++) {
     if (f[i] != 0) {
-      auto g = exp(log((f * f[i].inv()) >> i, n - int(i * k)) * k);
+      auto g = exp(k * log((f * f[i].inv()) >> i, n - int(i * k)));
       g *= f[i].pow(k);
       g <<= int(i * k);
       return g.prefix(n);
