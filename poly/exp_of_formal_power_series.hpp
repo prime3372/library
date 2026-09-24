f7f561643d58738f02d26739543b8a40a981ab49
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-25 04:20:22 +0900

    faster exp_of_formal_power_series.hpp

--- poly/exp_of_formal_power_series.hpp
+++ poly/exp_of_formal_power_series.hpp
@@ -8,10 +8,38 @@ namespace cp {
 template <class mint>
 formal_power_series<mint> exp(const formal_power_series<mint>& f, int n) {
   assert(f.empty() || f[0] == 0);
-  formal_power_series<mint> g = {1};
+  formal_power_series<mint> g = {1}, ig = {1};
   for (int k = 1; k < n; k *= 2) {
-    g *= 1 - log(g, 2 * k) + f.prefix(2 * k);
-    g.resize(2 * k);
+    auto z = g.prefix(2 * k);
+    internal::ntt(z);
+    auto iz = ig.prefix(2 * k);
+    internal::ntt(iz);
+
+    auto ig1 = z;
+    for (int i = 0; i < 2 * k; i++) ig1[i] *= iz[i];
+    internal::intt(ig1);
+    for (int i = 0; i < k; i++) ig1[i] = 0;
+    internal::ntt(ig1);
+    for (int i = 0; i < 2 * k; i++) ig1[i] *= iz[i];
+    internal::intt(ig1);
+    for (int i = k; i < 2 * k; i++) ig.emplace_back(-ig1[i]);
+
+    auto h = integral(diff(g) * ig).prefix(2 * k) - f.prefix(2 * k);
+    internal::ntt(h);
+    for (int i = 0; i < 2 * k; i++) h[i] *= z[i];
+    internal::intt(h);
+    for (int i = k; i < std::min(2 * k, n); i++) g.emplace_back(-h[i]);
+
+    auto ig2 = g.prefix(2 * k);
+    internal::ntt(ig2);
+    for (int i = 0; i < 2 * k; i++) ig2[i] *= iz[i];
+    internal::intt(ig2);
+    for (int i = 0; i < k; i++) ig2[i] = 0;
+    internal::ntt(ig2);
+    for (int i = 0; i < 2 * k; i++) ig2[i] *= iz[i];
+    internal::intt(ig2);
+    ig.resize(k);
+    for (int i = k; i < 2 * k; i++) ig.emplace_back(-ig2[i]);
   }
   return g.prefix(n);
 }
