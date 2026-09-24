0362ad6c755e0384d9fb3f6d426478d3f0d8095d
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-24 13:12:51 +0900

    a

--- poly/exp_of_formal_power_series.hpp
+++ poly/exp_of_formal_power_series.hpp
@@ -10,7 +10,7 @@ formal_power_series<mint> exp(const formal_power_series<mint>& f, int n) {
   assert(f.empty() || f[0] == 0);
   formal_power_series<mint> g = {1};
   for (int d = 1; d < n; d <<= 1) {
-    g = g * (1 - log(g, 2 * d) + f.prefix(2 * d));
+    g *= 1 - log(g, 2 * d) + f.prefix(2 * d);
     g.resize(2 * d);
   }
   return g.prefix(n);
