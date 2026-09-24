7315a8412f9c96c1bd9f7b65e49e9a0efbe801a8
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-24 13:48:12 +0900

    a

--- test/yosupo/enumerative_combinatorics/bell_number.test.cpp
+++ test/yosupo/enumerative_combinatorics/bell_number.test.cpp
@@ -12,6 +12,6 @@ using mint = modint998244353;
 int main() {
   int n;
   cin >> n;
-  auto a = bell<mint>(n);
-  for (int i = 0; i <= n; i++) cout << a[i] << " ";
+  auto b = bell<mint>(n);
+  for (int i = 0; i <= n; i++) cout << b[i] << " ";
 }
