6658809524c37541ba3b5d0717797176b17cfcc1
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-26 08:11:53 +0900

    use binom_mod to simplify implementation

--- poly/taylor_shift.hpp
+++ poly/taylor_shift.hpp
@@ -2,37 +2,27 @@
 
 #include <algorithm>
 
+#include "comb/binom_mod.hpp"
 #include "poly/formal_power_series.hpp"
 
 namespace cp {
 
 template <class mint>
 formal_power_series<mint> taylor_shift(formal_power_series<mint> f, mint c) {
-  constexpr int m = mint::mod();
   int n = int(f.size());
-  if (n <= 1) return f;
-
-  std::vector<mint> minv(n), ifact(n);
-  minv[1] = 1;
-  ifact[0] = ifact[1] = 1;
-  for (int i = 2; i < n; i++) {
-    minv[i] = -minv[m % i] * (m / i);
-    ifact[i] = ifact[i - 1] * minv[i];
-  }
-
+  binom_mod<mint> binom(n);
   formal_power_series<mint> g(n);
-  mint fact = 1, cpow = 1;
+  mint cpow = 1;
   g[0] = 1;
   for (int i = 1; i < n; i++) {
-    fact *= i;
     cpow *= c;
-    f[i] *= fact;
-    g[i] = cpow * ifact[i];
+    f[i] *= binom.fact(i);
+    g[i] = cpow * binom.ifact(i);
   }
   std::reverse(g.begin(), g.end());
   f *= g;
   f >>= n - 1;
-  for (int i = 0; i < n; i++) f[i] *= ifact[i];
+  for (int i = 0; i < n; i++) f[i] *= binom.ifact(i);
   return f;
 }
 
