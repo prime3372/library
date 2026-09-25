d74235c580ed7c47aa4f536b564f6bb5dec96cf8
prime3372 <syunrurusera@yahoo.ne.jp>
2026-09-25 09:27:49 +0900

    create stirling_second_fixed_k

---
+++ comb/stirling_second_fixed_k.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cassert>
+#include <vector>
+
+#include "comb/binom_mod.hpp"
+#include "poly/convolution.hpp"
+#include "poly/pow_of_formal_power_series.hpp"
+
+namespace cp {
+
+// https://en.wikipedia.org/wiki/Stirling_numbers_of_the_second_kind#Generating_functions
+// @return {S(k, k), S(k+1, k), ..., S(max_n, k)}
+template <class mint>
+std::vector<mint> stirling_second_fixed_k(int max_n, int k) {
+  assert(0 <= k && k <= max_n);
+  binom_mod<mint> binom(max_n);
+  formal_power_series<mint> f(max_n + 1);
+  for (int i = 1; i <= max_n; i++) f[i] = binom.ifact(i);
+  auto g = pow(f, k);
+  for (int i = k; i <= max_n; i++) {
+    g[i] *= binom.ifact(k);
+    g[i] *= binom.fact(i);
+  }
+  return g >> k;
+}
+
+}  // namespace cp
---
+++ test/yosupo/enumerative_combinatorics/stirling_number_of_the_second_kind_fixed_k.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM \
+  "https://judge.yosupo.jp/problem/stirling_number_of_the_second_kind_fixed_k"
+
+#include "comb/stirling_second_fixed_k.hpp"
+#include "util/static_modint.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cp;
+using mint = modint998244353;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  auto s = stirling_second_fixed_k<mint>(n, k);
+  for (mint& x : s) cout << x << " ";
+}
